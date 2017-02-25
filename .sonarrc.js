ac6f430f6f4c2de5553ba4d03eb861681e735cb9
molant <amolleda@gmail.com>
2017-02-24 21:41:16 -0800

    Added formatters and selection via config. Using collector from config and passing options to it. Added request module to jsdom collector (using the headers options). Clean up some of the todos and homogeneized code

--- .sonarrc.js
+++ .sonarrc.js
@@ -3,6 +3,18 @@ module.exports = {
     // "plugins": [
     //     "import"
     // ],
+    "collector": {
+        "jsdom": {
+            "headers": {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                "Accept-Language": "en-US,en;q=0.8,es;q=0.6,fr;q=0.4",
+                "Pragma": "no-cache",
+                "DNT": 1
+            },
+            waitFor: 5000
+        }
+    },
+    "formatter": "json",
     "rules": {
         "no-double-slash": "warning"
     }
