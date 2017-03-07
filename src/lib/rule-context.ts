3d95ad6c0545bbe80c2dd2d33dc68a499d98aa22
molant <amolleda@gmail.com>
2017-03-06 16:46:12 -0800

    Finish migration to TypeScript
    * Add ESLint TypeScript plugin.
    * Tweak rules to be more compatible with the current style
      and TypeScript.
    * Refactor code to require less `imports`, thus, generate
      less `no-unused-vars` errors.
    * Fix other linting issues.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Close #36

--- src/lib/rule-context.ts
+++ src/lib/rule-context.ts
@@ -3,9 +3,7 @@
  * @author Anton Molleda (@molant) based on Nicholas C. Zakas ESLint (https://github.com/eslint/eslint/blob/master/lib/rule-context.js)
  */
 
-import { validate as ruleValidator } from './config/config-rules';
-import { Severity, Location } from './types';
-import { Sonar } from './sonar';
+import { Sonar } from './sonar'; // eslint-disable-line no-unused-vars
 
 // ------------------------------------------------------------------------------
 // Helper functions
@@ -13,18 +11,25 @@ import { Sonar } from './sonar';
 
 // TODO: maybe this should go into another file for easier testing?
 /** Finds the Location of an HTMLElement in the document */
-const findElementLocation = (element: HTMLElement): Location => {
+const findElementLocation = (element: HTMLElement): ProblemLocation => {
+
     const html = element.ownerDocument.children[0].outerHTML;
     const occurrences = (html.match(new RegExp(element.outerHTML, 'g')) || []).length;
     let initHtml;
 
     if (occurrences === 1) {
+
         initHtml = html.substring(0, html.indexOf(element.outerHTML));
+
     } else if (occurrences > 1) {
+
         // TODO: return the right start place
         initHtml = html.substring(0, html.indexOf(element.outerHTML));
+
     } else {
+
         return null;
+
     }
 
     const lines = initHtml.split('\n');
@@ -33,6 +38,7 @@ const findElementLocation = (element: HTMLElement): Location => {
         column: lines.pop().length,
         line: lines.length + 1
     };
+
 };
 
 // ------------------------------------------------------------------------------
@@ -48,6 +54,7 @@ export class RuleContext {
     private sonar: Sonar
 
     constructor(ruleId: string, sonar: Sonar, severity: Severity | string, options, meta) {
+
         this.id = ruleId;
         this.options = options;
         this.meta = meta;
@@ -55,21 +62,27 @@ export class RuleContext {
         this.severity = typeof severity === 'string' ? Severity[severity] : severity;
 
         Object.freeze(this);
+
     }
 
     /** Reports a problem with the resource */
-    report(resource: string, nodeOrDescriptor: HTMLElement, message: string, location? : Location) {
+    report(resource: string, nodeOrDescriptor: HTMLElement, message: string, location?: ProblemLocation) {
+
         // TODO: this should probably contain the info of the resource (HTML, image, font, etc.)
         const descriptor = nodeOrDescriptor;
 
         let position;
 
         if (location !== null && descriptor.outerHTML) {
+
             position = findElementLocation(nodeOrDescriptor);
             position.column += location.column;
             position.line += location.line;
+
         } else {
+
             position = location;
+
         }
 
         this.sonar.report(
@@ -78,8 +91,8 @@ export class RuleContext {
             descriptor,
             position,
             message,
-            resource,
-            this.meta
+            resource
         );
+
     }
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,10 +1,5 @@
-/*
- * @fileoverview Interfaces and enums used in different files in Sonar that do not belong in an specific place.
- * @author Anton Molleda (@molant)
- */
-
 /** The builder of a given Rule */
-export interface RuleBuilder {
+interface RuleBuilder {
     /** Creates an instance of the rule. */
     create(config: Object): Rule;
     /** The metadata associated to the rule (docs, schema, etc.) */
@@ -16,44 +11,44 @@ export interface RuleBuilder {
 }
 
 /** A rule to be executed */
-export interface Rule {
+interface Rule {
     [key: string]: (...values: any[]) => void // TODO: this should be of type Listener, find a way to find it
 }
 
 /** The builder of a Collector */
-export interface CollectorBuilder {
+interface CollectorBuilder {
     (sonar, options): Collector,
 }
 
 /** A collector to be used by Sonar */
-export interface Collector {
+interface Collector {
     /** Collects all the information for the given target */
     collect(target: string): Promise<Array<Object>>;
     request;
 }
 
 /** A format function that will output the results obtained by Sonar */
-export interface Formatter {
+interface Formatter {
     format(problems: Array<Problem>): void
 }
 
 /** A specialized builder of plugins to be used by Sonar */
-export interface PluginBuilder {
+interface PluginBuilder {
     /** Creates an instance of the Plugin. */
     create(config: Object): Plugin;
 }
 
 /** A plugin that expands the collector's functionality */
-export interface Plugin {
+interface Plugin {
     // TBD
     any
 }
 
 /** A resource required by Sonar: Collector, Formatter, Plugin, Rule,  */
-export type Resource = CollectorBuilder | Formatter | PluginBuilder | RuleBuilder;
+type Resource = CollectorBuilder | Formatter | PluginBuilder | RuleBuilder; // eslint-disable-line no-unused-vars
 
 /** A problem found by a Rule in Sonar */
-export interface Problem {
+interface Problem {
     column: number,
     line: number,
     message: string,
@@ -63,14 +58,14 @@ export interface Problem {
 }
 
 /** The severity configuration of a Rule */
-export enum Severity {
+enum Severity {
     off = 0,
     warning = 1,
     error = 2
 }
 
 /** The location of a Problem in the code */
-export interface Location {
+interface ProblemLocation {
     column: number,
     line: number
 }
