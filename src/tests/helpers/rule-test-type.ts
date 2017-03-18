c126bc10e5e049a1410b3db6413e81f3d3652173
molant <amolleda@gmail.com>
2017-03-17 17:18:41 -0700

    Add `rule runner` test helper
    * Add new helper (`rule runner`) to make testing rules a lot easier.
    
      For now it only handles HTML fixtures (`element::nodename`), but
      expanding it should be fairly easy. A developer will only need to
      specify the name of the test, the events to receive (and where to
      get the data), and what is the expected output when testing a rule.
    
    * Refactor `lang-attribute` rule to use the new test rule runner.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Close #48

---
+++ src/tests/helpers/rule-test-type.ts
@@ -0,0 +1,25 @@
+import { ProblemLocation } from '../../lib/types';
+
+/** An event to fire while testing rules */
+export interface TestEvent {
+    /** The name of the event that should be fired.
+     *
+     * * For HTML elements, the form is `element::elementType::index` where
+     * index is optional and represents a number with the index of the element to return.
+     */
+    name: string,
+    /** The path to the fixture to use when sending the event */
+    fixture: string
+}
+
+export interface Report {
+    /** The message to validate */
+    message: string,
+    position?: ProblemLocation
+}
+
+export interface RuleTest {
+    name: string
+    events: Array<TestEvent>
+    report?: Report
+}
--- src/tests/lib/rules/lang-attribute/tests.ts
+++ src/tests/lib/rules/lang-attribute/tests.ts
@@ -1,65 +1,54 @@
-import * as jsdom from 'jsdom';
+/* eslint sort-keys: 0, no-undefined: 0 */
 import * as path from 'path';
-import * as pify from 'pify';
-import * as sinon from 'sinon';
-import test from 'ava';
 
-import { readFile } from '../../../../lib/util/misc';
-import { findProblemLocation } from '../../../../lib/util/location-helpers';
+import { Rule } from '../../../../lib/types'; // eslint-disable-line no-unused-vars
+import { RuleTest } from '../../../helpers/rule-test-type'; // eslint-disable-line no-unused-vars
+
+import * as ruleRunner from '../../../helpers/rule-runner';
 import * as rule from '../../../../lib/rules/lang-attribute/lang-attribute';
 
-import { Rule, RuleBuilder, ElementFoundEvent } from '../../../../lib/types'; // eslint-disable-line no-unused-vars
+const tests: Array<RuleTest> = [
+    {
+        name: `'lang' attribute is not specified fails`,
+        events: [{
+            name: 'element::html',
+            fixture: path.resolve(__dirname, './fixtures/no-lang-attribute.html')
+        }],
+        report: {
+            message: `'lang' attribute not specified on the 'html' element`,
+            position: undefined
+        }
+    },
+    {
+        name: `'lang' attribute is specified with no value fails`,
+        events: [{
+            name: 'element::html',
+            fixture: path.resolve(__dirname, './fixtures/lang-attribute-with-no-value.html')
+        }],
+        report: {
+            message: `empty 'lang' attribute specified on the 'html' element`,
+            position: { column: 7, line: 1 }
+        }
+    },
+    {
+        name: `'lang' attribute is specified and its value is an empty string fails`,
+        events: [{
+            name: 'element::html',
+            fixture: path.resolve(__dirname, './fixtures/lang-attribute-with-value-of-empty-string.html')
+        }],
+        report: {
+            message: `empty 'lang' attribute specified on the 'html' element`,
+            position: { column: 7, line: 1 }
+        }
+    },
+    {
+        name: `'lang' attribute is specified and its value is not an empty string passes`,
+        events: [{
+            name: 'element::html',
+            fixture: path.resolve(__dirname, './fixtures/lang-attribute-with-valid-value.html')
+        }]
+    }
 
-const getDOM = async (filePath) => {
-    return await pify(jsdom.env)(readFile(filePath));
-};
+];
 
-const runRule = async (t, filePath: string, tests): Promise<void> => {
-    const context = {
-        findProblemLocation: (element, content) => {
-            return findProblemLocation(element, {column: 0, line: 0}, content);
-        },
-        report: sinon.spy()
-    };
-
-    const window = await getDOM(path.join(__dirname, 'fixtures', filePath));
-    const instance = (<Rule>rule).create(context);
-    const eventData = { element: window.document.documentElement };
-
-    instance['element::html'](eventData);
-
-    tests(t, context, eventData);
-};
-
-test(`'lang' attribute is not specified`, runRule, 'no-lang-attribute.html', (t, context, eventData) => {
-    t.is(context.report.calledOnce, true);
-    t.is(context.report.calledWithExactly(
-        eventData.resource,
-        eventData.element,
-        `'lang' attribute not specified on the 'html' element`
-    ), true);
-});
-
-test(`'lang' attribute is specified with no value`, runRule, 'lang-attribute-with-no-value.html', (t, context, eventData) => {
-    t.is(context.report.calledOnce, true);
-    t.is(context.report.calledWithExactly(
-        eventData.resource,
-        eventData.element,
-        `empty 'lang' attribute specified on the 'html' element`,
-        {column: 7, line: 1}
-    ), true);
-});
-
-test(`'lang' attribute is specified and its value is an empty string`, runRule, 'lang-attribute-with-value-of-empty-string.html', (t, context, eventData) => {
-    t.is(context.report.calledOnce, true);
-    t.is(context.report.calledWithExactly(
-        eventData.resource,
-        eventData.element,
-        `empty 'lang' attribute specified on the 'html' element`,
-        {column: 7, line: 1}
-    ), true);
-});
-
-test(`'lang' attribute is specified and its value is not an empty string`, runRule, 'lang-attribute-with-valid-value.html', (t, context) => {
-    t.is(context.report.notCalled, true);
-});
+ruleRunner.testRule(<Rule>rule, tests);
