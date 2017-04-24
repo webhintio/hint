e4b77312df9e50a21ca058b92af7b53adf47967d
Cătălin Mariș <alrraa@gmail.com>
2017-04-24 07:17:58 +0300

    Add rule to check usage of X-Content-Type-Options
    Fix #22

---
+++ src/lib/rules/x-content-type-options/x-content-type-options.ts
@@ -0,0 +1,52 @@
+/**
+ * @fileoverview Check if responses are served with the
+ * `X-Content-Type-Options` HTTP response header.
+ */
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+import { IFetchEndEvent, IRule, IRuleBuilder } from '../../types'; // eslint-disable-line no-unused-vars
+import { RuleContext } from '../../rule-context'; // eslint-disable-line no-unused-vars
+
+// ------------------------------------------------------------------------------
+// Public
+// ------------------------------------------------------------------------------
+
+const rule: IRuleBuilder = {
+    create(context: RuleContext): IRule {
+
+        const validate = (fetchEnd: IFetchEndEvent) => {
+            const { element, resource, response } = fetchEnd;
+            const headerValue = (response.headers && response.headers['x-content-type-options']);
+
+            if (typeof headerValue === 'undefined') {
+                context.report(resource, element, `Resource served without the 'X-Content-Type-Options' HTTP response header`);
+
+                return;
+            }
+
+            if (headerValue.toLowerCase() !== 'nosniff') {
+                context.report(resource, element, `Resource served with invalid value ('${headerValue}') for the 'X-Content-Type-Options' HTTP response header`);
+            }
+        };
+
+        return {
+            'fetch::end': validate,
+            'targetfetch::end': validate
+        };
+    },
+    meta: {
+        docs: {
+            category: 'security',
+            description: `Require 'X-Content-Type-Options' HTTP response header`,
+            recommended: true
+        },
+        fixable: 'code',
+        schema: [],
+        worksWithLocalFiles: false
+    }
+};
+
+module.exports = rule;
