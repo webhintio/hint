1796a26cbcfe1ea2376143e804fe324c88cfc1a8
Cătălin Mariș <alrraa@gmail.com>
2017-05-04 18:07:10 +0300

    Automatically trigger docs rebuild in site repo
    Add code to automatically trigger a rebuild of the documentation in
    the repository of the website whenever something is changed in `docs/`.

---
+++ .travis/trigger-site-docs-build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# If something changed in `docs/` in the last 3 commits, trigger
+# a rebuild of the documentation in# the repository of the website.
+
+if ! git diff --quiet @~3..@ docs; then
+
+    # Triggering Travis CI builds
+    # https://docs.travis-ci.com/user/triggering-builds/
+
+    declare -r SITE_REPO="MicrosoftEdge%2Fsonarwhal.com"
+
+    declare -r BODY='{
+        "request": {
+            "message": "Rebuild docs (API request)",
+            "branch": "master",
+            "config": {
+                "script": "npm run rebuild-docs"
+            }
+        }
+    }'
+
+    curl -s -X POST \
+        -H "Content-Type: application/json; charset=utf-8" \
+        -H "Accept: application/json" \
+        -H "Travis-API-Version: 3" \
+        -H "Authorization: token ${TRAVIS_API_TOKEN}" \
+        -d "$BODY" \
+        "https://api.travis-ci.com/repo/${SITE_REPO}/requests"
+
+fi
