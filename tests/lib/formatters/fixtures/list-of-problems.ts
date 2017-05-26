e5207ac300f6133e2009fa75fb3c61d9345050ce
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2017-05-26 10:06:33 -0700

    Improvements to stylish formatter
    Stylish formatter now shows the line and column of an error when it is
    related to a DOM element.
    
    Fix #229

--- tests/lib/formatters/fixtures/list-of-problems.ts
+++ tests/lib/formatters/fixtures/list-of-problems.ts
@@ -42,26 +42,26 @@ const multipleproblemsandresources: Array<IProblem> = [{
     severity: Severity.warning
 },
 {
-    column: 1,
-    line: 10,
-    message: 'This is a problem in line 10',
+    column: -1,
+    line: -1,
+    message: 'This is a problem without line in myresource',
     resource: 'http://myresource.com/',
     ruleId: 'random-rule',
     severity: Severity.warning
 },
 {
-    column: 1,
-    line: 5,
-    message: 'This is a problem in line 5',
-    resource: 'http://myresource2.com/',
+    column: -1,
+    line: -1,
+    message: 'This is a problem without line',
+    resource: 'http://myresource2.com/this/resource/is/really/really/long/resources/image/imagewithalongname.jpg',
     ruleId: 'random-rule',
     severity: Severity.error
 },
 {
-    column: 1,
-    line: 1,
-    message: 'This is a problem in line 1 column 1',
-    resource: 'http://myresource2.com/',
+    column: -1,
+    line: -1,
+    message: 'This is another problem without line',
+    resource: 'http://myresource2.com/this/resource/is/really/really/long/resources/image/imagewithalongname.jpg',
     ruleId: 'random-rule',
     severity: Severity.warning
 }];
