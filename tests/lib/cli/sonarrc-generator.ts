66c2be2ec499e88a87fe6490ed493bed0d9a339e
Anton Molleda <antonmo@microsoft.com>
2017-06-18 20:06:10 -0700

    Change how sonar resources are loaded
    The current order is:
    
    1. Resources in core
    2. Scoped packages (@sonarwhal/XXX)
    3. Prefixed packages (sonar-resourceType-XXX)
    
    The previous way of loading resources was weak and is better to rely
    on `require` when dealing with external resources.
    
    Some improvements of the current code over the previous:
    
    * We load only the resources that we need instead of all, and on demand.
    * `sonar.create()` is now sync and should load faster.
    * Validation of rules is done now in `sonar` (when loading them individually
      and not the `cli`. This fixes a previously unkown issue on `rule-runner`
      where the schema of rules wasn't validated (it showed up in `ssllabs`.
    * Remove all code related to `plugins` until we know how they look like.
    * Refactor where the code to generate a valid `.sonarrc` is.
    * Remove `sonar.create()` in favor of `new Sonar()`.
    * Clean up several tests and added @types/sinon.
    * Refactor `ui` to `cli` and move all `generators` there.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Fix #234

---
+++ tests/lib/cli/sonarrc-generator.ts
@@ -0,0 +1,150 @@
+import * as proxyquire from 'proxyquire';
+import * as sinon from 'sinon';
+import test from 'ava';
+
+const inquirer = { prompt() { } };
+const resourceLoader = {
+    getCoreCollectors() { },
+    getCoreFormatters() { },
+    getCoreRules() { },
+    loadRules() { }
+};
+
+const promisifyObject = { promisify() { } };
+
+const stubUtilObject = {
+    promisify() {
+        return promisifyObject.promisify;
+    }
+};
+
+proxyquire('../../../src/lib/cli/sonarrc-generator', {
+    '../utils/resource-loader': resourceLoader,
+    inquirer,
+    util: stubUtilObject
+});
+
+import { initSonarrc } from '../../../src/lib/cli/sonarrc-generator';
+
+test.beforeEach((t) => {
+    sinon.stub(promisifyObject, 'promisify').resolves();
+    sinon.spy(stubUtilObject, 'promisify');
+
+    t.context.util = stubUtilObject.promisify;
+    t.context.promisify = promisifyObject.promisify;
+});
+
+test.afterEach.always((t) => {
+    t.context.util.restore();
+    t.context.promisify.restore();
+});
+
+const collectors = [
+    'collector1',
+    'collector2'];
+
+const rules = ['rule1', 'rule2'];
+
+const rulesData = new Map([
+    ['rule1', {
+        meta: {
+            docs: { description: 'description rule 1' },
+            recommended: false
+        }
+    }],
+    ['rule2', {
+        meta: {
+            docs: { description: 'description rule 2' },
+            recommended: true
+        }
+    }]
+]);
+
+const formatters = [
+    'formatter1',
+    'formatter2'];
+
+test.serial(`generate should call to "inquirer.prompt" with the right data`, async (t) => {
+    const sandbox = sinon.sandbox.create();
+
+    sandbox.stub(resourceLoader, 'getCoreCollectors').returns(collectors);
+    sandbox.stub(resourceLoader, 'getCoreFormatters').returns(formatters);
+    sandbox.stub(resourceLoader, 'getCoreRules').returns(rules);
+    sandbox.stub(resourceLoader, 'loadRules').returns(rulesData);
+    sandbox.stub(inquirer, 'prompt').resolves({
+        collector: '',
+        default: '',
+        formatter: '',
+        rules: []
+    });
+
+    await initSonarrc();
+
+    const questions = (inquirer.prompt as sinon.SinonStub).args[0][0];
+    const rulesKeys = rules;
+
+    t.is(questions[0].choices.length, collectors.length);
+    t.is(questions[1].choices.length, formatters.length);
+    t.is(questions[2].choices.length, 2);
+    t.is(questions[3].choices.length, rulesKeys.length);
+    t.is(questions[3].choices[0].value, rulesKeys[0]);
+    t.is(questions[3].choices[0].name, `${rulesKeys[0]} - ${rulesData.get(rulesKeys[0]).meta.docs.description}`);
+    t.is(questions[3].choices[1].value, rulesKeys[1]);
+    t.is(questions[3].choices[1].name, `${rulesKeys[1]} - ${rulesData.get(rulesKeys[1]).meta.docs.description}`);
+
+    sandbox.restore();
+});
+
+test.serial(`generate should call to "fs.writeFile" with the right data`, async (t) => {
+    const sandbox = sinon.sandbox.create();
+    const questionsResults = {
+        collector: 'cdp',
+        default: false,
+        formatter: 'json',
+        rules: ['rule1']
+    };
+
+    sandbox.stub(resourceLoader, 'getCoreCollectors').returns(collectors);
+    sandbox.stub(resourceLoader, 'getCoreFormatters').returns(formatters);
+    sandbox.stub(resourceLoader, 'getCoreRules').returns(rules);
+    sandbox.stub(resourceLoader, 'loadRules').returns(rulesData);
+    sandbox.stub(inquirer, 'prompt').resolves(questionsResults);
+
+    await initSonarrc();
+
+    const fileData = JSON.parse(t.context.promisify.args[0][1]);
+
+    t.is(fileData.collector.name, questionsResults.collector);
+    t.is(fileData.formatter, questionsResults.formatter);
+    t.is(fileData.rules.rule1, 'error');
+    t.is(fileData.rules.rule2, 'off');
+
+    sandbox.restore();
+});
+
+test.serial(`if the user choose to use the default rules configuration, all recommended rules should be set to "error" in the configuration file`, async (t) => {
+    const sandbox = sinon.sandbox.create();
+    const questionsResults = {
+        collector: 'cdp',
+        default: true,
+        formatter: 'json',
+        rules: []
+    };
+
+    sandbox.stub(resourceLoader, 'getCoreCollectors').returns(collectors);
+    sandbox.stub(resourceLoader, 'getCoreFormatters').returns(formatters);
+    sandbox.stub(resourceLoader, 'getCoreRules').returns(rules);
+    sandbox.stub(resourceLoader, 'loadRules').returns(rulesData);
+    sandbox.stub(inquirer, 'prompt').resolves(questionsResults);
+
+    await initSonarrc();
+
+    const fileData = JSON.parse(t.context.promisify.args[0][1]);
+
+    t.is(fileData.collector.name, questionsResults.collector);
+    t.is(fileData.formatter, questionsResults.formatter);
+    t.is(fileData.rules.rule2, 'error');
+    t.is(fileData.rules.rule1, 'off');
+
+    sandbox.restore();
+});
