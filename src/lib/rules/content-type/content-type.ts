1369f497d2b7fdd73b010dc3a14ea9b8301ecacb
Cătălin Mariș <alrraa@gmail.com>
2017-10-16 14:14:58 +0300

    Chore: Remove overwrite from `content-type` rule
    `mime-db` now includes¹ the appropriate font media types, thus,
    there is no longer a need to overwrite² the font media types.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    ¹ https://github.com/jshttp/mime-db/pull/90
      https://github.com/jshttp/mime-db/pull/77
    
    ² https://github.com/sonarwhal/sonar/commit/e35b778004be3038d0b994f9a258dd454b994622

--- src/lib/rules/content-type/content-type.ts
+++ src/lib/rules/content-type/content-type.ts
@@ -191,29 +191,6 @@ const rule: IRuleBuilder = {
             return null;
         };
 
-        const overwriteMediaType = (mediaType: string): string => {
-
-            /*
-             * TODO: The following is done because of a current limitation in `mime-db`.
-             * https://github.com/jshttp/mime-db/issues/20
-             */
-
-            switch (mediaType) {
-                case 'application/x-font-otf':
-                    return 'font/otf';
-                case 'application/font-sfnt':
-                    return 'font/sfnt';
-                case 'application/x-font-ttf':
-                    return 'font/ttf';
-                case 'application/font-woff':
-                    return 'font/woff';
-                case 'application/font-woff2':
-                    return 'font/woff2';
-                default:
-                    return mediaType;
-            }
-        };
-
         const validate = async (fetchEnd: IFetchEnd) => {
             const { element, resource, response }: { element: IAsyncHTMLElement, resource: string, response: IResponse } = fetchEnd;
 
@@ -271,12 +248,10 @@ const rule: IRuleBuilder = {
 
             // Try to determine the media type and charset of the resource.
 
-            let mediaType: string =
+            const mediaType: string =
                 determineMediaTypeBasedOnElement(element) ||
                 determineMediaTypeBasedOnFileType(response.body.rawContent) ||
                 determineMediaTypeBasedOnFileExtension(resource);
-
-            mediaType = overwriteMediaType(mediaType);
 
             const charset: string = determineCharset(mediaType, originalMediaType);
 
