0be30678acc7c443c792dde88335c3ed4735fd71
Antón Molleda <amolleda@gmail.com>
2017-11-01 21:05:50 -0700

    Fix: Debugging Protocol Connector improvements
    * Add `verbose` mode to `chrome-launcher` when in `debug` mode
    * Not emit `scan::end` if there's been a problem connecting to the
      browser in `debugging-protocol-connector`
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Ref: #603

--- src/lib/connectors/chrome/chrome-launcher.ts
+++ src/lib/connectors/chrome/chrome-launcher.ts
@@ -115,6 +115,7 @@ export class CDPLauncher extends Launcher {
 
             const chrome: chromeLauncher.LaunchedChrome = await chromeLauncher.launch({
                 chromeFlags,
+                logLevel: debug.enabled ? 'verbose' : 'silent',
                 startingUrl: url
             });
 
