3d8d9980dbfbda821d45b2ea50e080fd6eb6d953
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2017-11-28 11:51:18 -0800

    Fix: Fix eslint issues.

--- src/lib/utils/misc.ts
+++ src/lib/utils/misc.ts
@@ -188,24 +188,27 @@ const findPackageRoot = (dirname: string = __dirname, fileToFind: string = 'pack
 /**
  * Find the node_modules folder where sonarwhal is installed as
  * a dependency or returns the sonarwhal node_modules folder if not.
- * @param dirname Starting directory.
  */
 const findNodeModulesRoot = (dirname: string = __dirname): string => {
     const packageRoot = findPackageRoot(dirname);
 
     const nodeModulesPath = path.join(packageRoot, '..');
 
-    // If sonarwhal is installed as a dependency
-    // then we need to return the parent folder
-    // i.e. c:\myproject\node_modules\sonarwhal -> c:\myproject\node_modules
+    /*
+     * If sonarwhal is installed as a dependency
+     * then we need to return the parent folder
+     * i.e. c:\myproject\node_modules\sonarwhal -> c:\myproject\node_modules
+     */
     if (nodeModulesPath.endsWith('node_modules')) {
         return nodeModulesPath;
     }
 
-    // If we are using directly the sonarwhal project
-    // then we need to return the node_modules folder
-    // inside sonarwhal
-    // i.e. c:\sonarwhal -> c:\sonarwhal\node_modules
+    /*
+     * If we are using directly the sonarwhal project
+     * then we need to return the node_modules folder
+     * inside sonarwhal
+     * i.e. c:\sonarwhal -> c:\sonarwhal\node_modules
+     */
     return path.join(packageRoot, 'node_modules');
 };
 
