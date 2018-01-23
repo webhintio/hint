6ae8df828c582b8eb40d03342ec8faf26ef8debf
Cătălin Mariș <alrraa@gmail.com>
2018-01-22 22:26:42 -0800

    Breaking: Limit `X-Content-Type-Options` usage
    Change `x-content-type-options` rule so that it limits the usage
    of the `X-Content-Type-Options` header to scripts and stylesheets
    as modern browsers actually only respect the header for those
    types of resources¹.
    
    Also, sending the header for resources such as images, creates
    problems² in some older browsers.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    ¹ https://fetch.spec.whatwg.org/#x-content-type-options-header
    ² https://github.com/whatwg/fetch/issues/395
    
    Fix #767

--- tests/helpers/test-server.ts
+++ tests/helpers/test-server.ts
@@ -281,7 +281,6 @@ export class Server {
                 res.status(200);
                 res.setHeader('Content-Length', '0');
                 res.setHeader('Content-Type', 'image/x-icon');
-                res.setHeader('X-Content-Type-Options', 'nosniff');
                 res.end();
             });
         }
