48c95f82889aabf6641a1b0e55620e65d4e2193a
molant <amolleda@gmail.com>
2018-01-30 19:21:57 -0800

    New: Rule `performance-budget`
    Fix #790

---
+++ docs/user-guide/rules/performance-budget.md
@@ -0,0 +1,147 @@
+# Performance budget (`performance-budget`)
+
+> A web performance budget is a group of limits to certain values that affect
+> site performance that should not be exceeded in the design and development of
+> any web project. This could be the total size of a page, size of images you
+> are uploading, or even the number of HTTP requests that your webpage
+> generates.
+
+https://www.keycdn.com/blog/web-performance-budget/
+
+## Why is this important?
+
+As of January 2018, the average size of a website is 3,545kB:
+
+![image](https://chart.googleapis.com/chart?chs=400x225&cht=p&chco=007099&chd=t:1818,70,98,504,120,851,27&chds=0,1818&chdlp=b&chdl=total%203545%20kB&chl=Images+-+1818+kB%7CHTML+-+70+kB%7CStylesheets+-+98+kB%7CScripts+-+504+kB%7CFonts+-+120+kB%7CVideo+-+851+kB%7COther+-+27+kB&chma=|5&chtt=Average+Bytes+per+Page+by+Content+Type)
+
+Although the global average connection is 7.2Mb/s (check [_Akamai's state of
+the Internet 2017_][state of the internet]), "no bit is faster than one that is
+not sent" (quote by [Ilya Grigorik][faster bit]). Web developers need to be
+mindful not only about the size of their sites, but also the number of
+requests, different domains, third party scripts, etc. The time required by a
+browser to download a 200kB file is not the same than 20 files of 10kB.
+
+## What does the rule check?
+
+This rule calculates how long it will take to download all the resources loaded
+initially by the website under a `3G Fast` network. If the load time is
+**greater than 5 seconds**, the rule will fail.
+
+To calculate the final load time, some assumptions and simplifications are
+done. While the real numbers might be different, the results should provide
+enough guidance to know if something needs more attention.
+
+The reason for using predefined conditions and assumptions are:
+
+* Guarantee consistent results accross runs. If a website serves the same
+  assets, the results should be the same.
+* Show the impact in load time of each transmitted byte with the goal of
+  reducing the number and size of resources downloaded (first and third party).
+
+The simplified formula to calculate the time is:
+
+```text
+Time = (total number of requests * RTT) +
+       (number of different domains * RTT) +
+       (number of different secured domains * RTT) +
+       (number of redirects * RTT) +
+       (total number of requests * TCP slow-start phase) +
+       (total size of resources / bandwidth)
+```
+
+This is the list of things taken into account:
+
+* Everything is a first load, no values are cached, and no connections are
+  opened.
+* [`RTT` (Round-Trip Time)][rtt] is fixed and changes depending on the
+  configured network (more about this in #). It assumes all servers respond
+  instantly and in the same amount of time.
+* [`DNS lookup`][dns lookup]: Every hostname resolution requires 1 RTT,
+  imposing latency on the request and blocking the request while the lookup is
+  in progress.
+* [`TCP handshake`][three-way handshake]: Each request requires a new TCP
+  connection. TCP connections require 1 RTT before start sending information to
+  the server. There's no connection reuse and the maximum number of connections
+  to a domain (usually 6) is ignored.
+* [`TCP slow-start phase`][slow-start phase]: The values used to calculate the
+  duration are:
+  * `cwnd`: 10 network segments
+  * `rwnd`: 65,535 bytes (no `TCP window scaling`)
+  * `segment size`: 1460 bytes
+  After this phase, the full bandwidth of the connection is used to download
+  the remaining.
+* [`TLS handshake`][tls handshake]: New TLS connections usually require two
+  roundtrips for a "full handshake". However, there are ways of requiring only
+  1 RTT like [`TLS False Start`][tls false start] and [`TLS Session
+  Resumption`][tls session resumption]. This rule assumes the optimistic
+  scenario.
+* Redirects: We simplify the redirects and assume the connection is reused and
+  to the same server. If the redirect is to another domain, the penalty will be
+  even greater.
+* The server doesn't use [`HTTP2`][http2].
+
+## Can the rule be configured?
+
+You can change the type of connection and/or the target load time as follows:
+
+```json
+{
+    "performance-budget": ["error", {
+        "connectionType": "Dial",
+        "loadTime": 10
+    }]
+}
+```
+
+The possible values and the associated speeds for `connectionType` are:
+
+| Value  |       In |      Out |   RTT |
+| -------|----------|----------|-------|
+| FIOS   |  20 Mbps |   5 Mbps |   4ms |
+| LTE    |  12 Mbps |  12 Mbps |  70ms |
+| 4G     |   9 Mbps |   9 Mbps | 170ms |
+| Cable  |   5 Mbps |   1 Mbps |  28ms |
+| 3G     | 1.6 Mbps | 768 Kbps | 300ms |
+| 3GFast | 1.6 Mbps | 768 Kbps | 150ms |
+| DSL    | 1.5 Mbps | 384 Kbps |  50ms |
+| 3GSlow | 400 Kbps | 400 Kbps | 400ms |
+| 3G_EM  | 400 Kbps | 400 Kbps | 400ms |
+| 2G     | 280 Kbps | 256 Kbps | 800ms |
+| Edge   | 240 Kbps | 200 Kbps | 840ms |
+| Dial   |  49 Kbps |  30 Kbps | 120ms |
+
+`loadTime` has to be a number greater than `1` and indicates the time in
+seconds to load all the resources.
+
+### Examples that **trigger** the rule
+
+Assuming a `3G Fast` connection, the following will fail:
+
+* A site with 2 redirects and 2 images over 362KB each
+* A site with just HTML, loaded over https, with a size over 1MB
+
+### Examples that **pass** the rule
+
+* A site with 2 images smaller than 350KB
+* A site with just HTML, loaded over https, with a size less than 900KB
+
+## Further Reading
+
+* https://hpbn.co/
+* https://infrequently.org/2017/10/can-you-afford-it-real-world-web-performance-budgets/
+* https://www.keycdn.com/blog/web-performance-budget/
+* https://medium.com/dev-channel/the-cost-of-javascript-84009f51e99e
+
+<!-- Link labels -->
+
+[dns lookup]: https://www.cloudflare.com/learning/dns/what-is-dns/
+[faster bit]: https://hpbn.co/building-blocks-of-tcp/#tuning-application-behavior
+[http2]: https://hpbn.co/http2/
+[rtt]: https://hpbn.co/primer-on-latency-and-bandwidth/#speed-of-light-and-propagation-latency
+[state of the internet]: https://www.akamai.com/us/en/multimedia/documents/state-of-the-internet/q1-2017-state-of-the-internet-connectivity-report.pdf
+[tcp handshake]: https://hpbn.co/building-blocks-of-tcp/#three-way-handshake
+[three-way handshake]: https://hpbn.co/building-blocks-of-tcp/#three-way-handshake
+[slow-start phase]: https://hpbn.co/building-blocks-of-tcp/#slow-start
+[tls handshake]: https://hpbn.co/transport-layer-security-tls/#tls-handshake
+[tls false start]: https://hpbn.co/transport-layer-security-tls/#enable-tls-false-start
+[tls session resumption]: https://hpbn.co/transport-layer-security-tls/#tls-session-resumption
---
+++ src/lib/rules/performance-budget/performance-budget.ts
@@ -0,0 +1,284 @@
+/**
+ * @fileoverview Performance budget checks if your site will load fast enough based on the size of your resources and a given connection speed
+ */
+
+import * as url from 'url';
+
+import { Category } from '../../enums/category';
+import { RuleContext } from '../../rule-context';
+// The list of types depends on the events you want to capture.
+import { IRule, IRuleBuilder, IFetchEnd, IScanEnd, IResponse } from '../../types';
+import { debug as d } from '../../utils/debug';
+
+import { NetworkConfig, ResourceResponse, PerfBudgetConfig } from './types';
+import * as Connections from './connections';
+
+const debug: debug.IDebugger = d(__filename);
+
+/**
+ * The default configuration for this rule:
+ *
+ * * `connectionType`: 3GFast
+ * * `loadTime`: 5
+ *
+ */
+const defaultConfig: PerfBudgetConfig = {
+    connectionType: '3GFast',
+    loadTime: 5
+};
+
+/*
+ * ------------------------------------------------------------------------------
+ * Public
+ * ------------------------------------------------------------------------------
+ */
+
+const rule: IRuleBuilder = {
+    create(context: RuleContext): IRule {
+
+        /** An array containing all the responses. */
+        const responses: Array<ResourceResponse> = [];
+        /** Set with all the different domains loaded by the site. */
+        const uniqueDomains: Set<string> = new Set();
+        /** Set with all the different https domains loaded by the site. */
+        const secureDomains: Set<string> = new Set();
+        /** Number of total redirects found to load the resources. */
+        let performedRedirects: number = 0;
+        /** Number of total requests performed. */
+        let performedRequests: number = 0;
+
+        /** Update the stored information for unique domains (`uniqueDomains`) and connections over https (`secureDomains`). */
+        const updateDomainsInfo = (resource: string) => {
+            const resourceUrl = url.parse(resource);
+
+            uniqueDomains.add(resourceUrl.hostname);
+
+            if (resourceUrl.protocol === 'https:') {
+                secureDomains.add(resourceUrl.hostname);
+            }
+        };
+
+        /** Updates the total count of redirects (`performedRedirects`) for the given response and the total number of requests. */
+        const updateCounters = (response: IResponse) => {
+            performedRedirects += response.hops.length;
+            performedRequests++;
+        };
+
+        /**
+         * Store the amount of bytes transferred for a given response into
+         * `responses`. It takes into account the mime type, compressed,
+         * and uncompressed size.
+         */
+        const updateSizes = async (resource: string, response: IResponse) => {
+            const uncompressedSize: number = response.body.rawContent ?
+                response.body.rawContent.byteLength :
+                response.body.content.length;
+            let sentSize: number;
+
+            try {
+                sentSize = (await response.body.rawResponse()).byteLength;
+            } catch (e) {
+                debug(`Error trying to get the rawResponse for ${resource}. Using uncompressedSize instead`);
+                debug(e);
+
+                sentSize = uncompressedSize;
+            }
+
+            responses.push({
+                resource,
+                sentSize,
+                uncompressedSize
+            });
+        };
+
+        const onFetchEnd = async (fetchEnd: IFetchEnd) => {
+            debug(`Validating rule Performance budget`);
+            const { resource, response } = fetchEnd;
+
+            updateDomainsInfo(resource);
+            updateCounters(response);
+            await updateSizes(resource, response);
+        };
+
+        /** Returns the details for the selected network configuration or the default one. */
+        const getConfiguration = (): NetworkConfig => {
+            const userConfig = Object.assign({}, defaultConfig, context.ruleOptions) as PerfBudgetConfig;
+            const config = Connections.getById(userConfig.connectionType);
+
+            config.load = userConfig.loadTime;
+
+            return config;
+        };
+
+        /** Calculates the minimum required time to do all the DNS Look ups for the loaded resources. */
+        const calculateTotalDNSLookUp = (domains: Set<string>, config: NetworkConfig): number => {
+            // Perfect scenario for a DNS Lookup is 1 round trip in cold
+
+            // TODO: Find a link to document this
+            const dnsLookUpTime = config.latency;
+            const total = domains.size * dnsLookUpTime / 1000;
+
+            debug(`Total DNS lookup time: ${total}`);
+
+            return total;
+        };
+
+        /**
+         * Calculates the minimum required time to establish all the TCP connections.
+         *
+         * All TCP beging with a _Three-way handshake_, but clients can start sending application data
+         * after they receive the `ACK` message, si the delay is just 1 RTT:
+         *
+         * `time = connections * RTT`
+         *
+         * You can read more about this in: https://hpbn.co/building-blocks-of-tcp/#three-way-handshake
+         *
+         */
+        const calculateTotalTCPHandshake = (connections: number, config: NetworkConfig): number => {
+            const time = connections * config.latency / 1000;
+
+            return time;
+        };
+
+        /** Calculates the minimum required time to do all the TLS handshaing of a website. */
+        const calculateTotalTLSHandshaking = (domains: Set<string>, config: NetworkConfig): number => {
+            // Perfect scenario for a TLS Handshake is 1 round trip in cold
+
+            // TODO: Find a link to document this
+            const tlsHandshakingTime = config.latency;
+            const total = domains.size * tlsHandshakingTime / 1000;
+
+            debug(`Total TLS handshake time: ${total}`);
+
+            return total;
+        };
+
+        /** Calculates the minimum required time to process all the redirects found. */
+        const calculateTotalRedirectTime = (redirects: number, config: NetworkConfig) => {
+            // Perfect scenario for a redirect is 1 round trip in cold
+            const total = redirects * config.latency / 1000;
+
+            debug(`Total redirect time: ${total}`);
+
+            return total;
+        };
+
+        /**
+         * Calculates the transfer time for a resource under the following assumptions:
+         *
+         * * Connections use the maximum bandwidth after `slow-start`
+         * * The congestion window (`cwnd`) is 10 network segments
+         * * The receive window (`rwnd`) is 65,535 bytes and there isn't any `TCP window scaling`
+         * * There's a `slow-start` phase
+         * * DNS lookup, and TLS handshake are not taken into account here
+         *
+         * The way the time it's calculated is as follows:
+         *
+         * 1. Calculate the `slow-start` time for the minimum of `rwnd` or the transfered size.
+         * 2. If the size of the response is bigger than `rwnd`, the rest of the file is sent at maximum speed.
+         *
+         * To know more about `slow-start` visit https://hpbn.co/building-blocks-of-tcp/#slow-start
+         */
+        const calculateTransferTimeForResource = (response: ResourceResponse, config: NetworkConfig): number => {
+            const networkSegmentSize: number = 1460;
+            const rwnd: number = 65535;
+            const cwnd: number = 10; // RFC-6928: https://tools.ietf.org/html/rfc6928
+            const dataInFlight: number = Math.min(response.sentSize, rwnd);
+            const segments: number = Math.ceil(dataInFlight / networkSegmentSize);
+
+            const time = config.latency * (Math.log2(1 + segments / cwnd)) / 1000;
+
+            if (response.sentSize < rwnd) {
+                return time;
+            }
+
+            // `bwIn` is measured in bits per second, thus bytes * 8 to get the bits
+            return (response.sentSize - rwnd) * 8 / config.bwIn + time;
+        };
+
+        /** Calculates the transfer time for all the given responses with no TCP connection reuse.*/
+        const calculateTransferTimeWithSlowStart = (allResponses: Array<ResourceResponse>, config: NetworkConfig): number => {
+            const totalTime = allResponses.reduce((time, resource) => {
+                const transfertTime = calculateTransferTimeForResource(resource, config);
+
+                return time + transfertTime;
+            }, 0);
+
+            return totalTime;
+        };
+
+        /**
+         * Returns the total time in seconds to load all the resources for the
+         * best case scenario in the configured network taking into account:
+         *
+         * * DNS Lookup
+         * * TCP Handshake
+         * * TLS Handshake
+         * * Redirects
+         * * Size of resources
+         */
+        const getBestCaseScenario = (config: NetworkConfig) => {
+
+            const dnsLookUpTime = calculateTotalDNSLookUp(uniqueDomains, config);
+            const tcpHandshakeTime = calculateTotalTCPHandshake(performedRequests, config);
+            const tlsHandshakeTime = calculateTotalTLSHandshaking(secureDomains, config);
+            const redirectTime = calculateTotalRedirectTime(performedRedirects, config);
+            const transferTimeSlowStart = calculateTransferTimeWithSlowStart(responses, config);
+
+            const total = dnsLookUpTime +
+                tcpHandshakeTime +
+                tlsHandshakeTime +
+                redirectTime +
+                transferTimeSlowStart;
+
+            return total;
+        };
+
+        /**
+         * Calculates if the size of all the loaded resources is small enough to
+         * load the site in the allocated time.
+         */
+        const onScanEnd = async (scanEnd: IScanEnd) => {
+            const { resource } = scanEnd;
+            const config: NetworkConfig = getConfiguration();
+            const loadTime = getBestCaseScenario(config);
+
+            debug(`Ideal load time: ${loadTime}s`);
+
+            if (loadTime > config.load) {
+                await context.report(resource, null, `To load all the resources on a ${config.id} network, it will take about ${loadTime.toFixed(1)}s in optimal conditions.
+That's ${(loadTime - config.load).toFixed(1)}s more than the ${config.load}s target.`);
+            }
+        };
+
+        return {
+            'fetch::end': onFetchEnd,
+            'scan::end': onScanEnd,
+            'targetfetch::end': onFetchEnd
+        };
+    },
+    meta: {
+        docs: {
+            category: Category.performance,
+            description: `Performance budget checks if your site will load fast enough based on the size of your resources and a given connection speed`
+        },
+        recommended: true,
+        schema: [{
+            additionalProperties: false,
+            properties: {
+                connectionType: {
+                    oneOf: [{ enum: Connections.ids }],
+                    type: 'string'
+                },
+                loadTime: {
+                    minimum: 1,
+                    type: 'number'
+                }
+            },
+            type: 'object'
+        }],
+        worksWithLocalFiles: false
+    }
+};
+
+module.exports = rule;
