4f67e43bb7444cb179e1e5c9d95ea08333816de1
molant <amolleda@gmail.com>
2018-02-16 14:24:33 -0800

    Fix: Update variable script

--- scripts/set-NPM_CONFIG_PREFIX.bat
+++ scripts/set-NPM_CONFIG_PREFIX.bat
@@ -1,2 +1,3 @@
-rem yarn has issues with amphtml-validator on windows bypassed with this (https://github.com/yarnpkg/yarn/issues/4918#issuecomment-355712632)
-for /f "delims=" %%A in ('node -e console.log^(process.execPath^)') do @set "NPM_CONFIG_PREFIX=%%A"
+@echo off
+REM yarn has issues with amphtml-validator on windows bypassed with this (https://github.com/yarnpkg/yarn/issues/4918#issuecomment-355712632)
+for /f "delims=" %%A in ('node -e console.log^(require^(^'path^'^).dirname^(process.execPath^)^)') do @set "NPM_CONFIG_PREFIX=%%A"
