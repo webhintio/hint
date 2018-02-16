87798d53bbe4b8eeafe0034ee6885c6fbf06fc9e
molant <amolleda@gmail.com>
2018-02-16 13:54:01 -0800

    Chore: Issue with `amphtml-validator` and Windows
    Fix #820

---
+++ scripts/set-NPM_CONFIG_PREFIX.bat
@@ -0,0 +1,3 @@
+@echo off
+REM yarn has issues with amphtml-validator on windows bypassed with this (https://github.com/yarnpkg/yarn/issues/4918#issuecomment-355712632)
+for /f "delims=" %%A in ('node -e console.log^(require^(^'path^'^).dirname^(process.execPath^)^)') do @set "NPM_CONFIG_PREFIX=%%A"
