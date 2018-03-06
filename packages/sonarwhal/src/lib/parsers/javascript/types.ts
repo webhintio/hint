4a24bd4d681baec6d79ed70462e4f36782657fdd
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2018-03-06 10:48:31 -0800

    Chore: Remove unused code

--- packages/sonarwhal/src/lib/parsers/javascript/types.ts
+++
@@ -1,7 +0,0 @@
-import { Event } from '../../types';
-
-/** The object emitted by the `javascript` parser */
-export type ScriptParse = Event & {
-    /** The source code parsed */
-    sourceCode: any;
-};
