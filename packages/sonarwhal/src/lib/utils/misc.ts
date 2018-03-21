b7f4cc6c9674b0c32e1cabcb6195ab0f98c7e8eb
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2018-03-21 16:52:18 -0700

    Fix: Address PR comments

--- packages/sonarwhal/src/lib/utils/misc.ts
+++ packages/sonarwhal/src/lib/utils/misc.ts
@@ -143,9 +143,13 @@ const isRegularProtocol = (uri: string): boolean => {
     const normalizedUri = normalizeString(uri);
     const exec = protocolRegex.exec(normalizedUri);
     const protocol = exec ? exec[1] : null;
+
     /*
      * Ignore cases such as `javascript:void(0)`,
      * `data:text/html,...`, `file://` etc.
+     *
+     * Note: `null` is when the protocol is not
+     * specified (e.g.: test.html).
      */
 
     if (![null, 'http', 'https'].includes(protocol)) {
