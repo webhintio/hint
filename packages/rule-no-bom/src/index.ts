27347f8efde08e9a366fbc38c65cb285551a3f0d
molant <amolleda@gmail.com>
2018-03-22 16:05:27 -0700

    New: Add `no-bom` rule
    Add rule to checks if a text file starts with the BOM character.
    
    Fix #371
    Close #919

---
+++ packages/rule-no-bom/src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @fileoverview Warns agains using the BOM character at the beginning of a text file
+ */
+
+import * as noBom from './no-bom';
+
+module.exports = { 'no-bom': noBom };
---
+++ packages/rule-no-bom/tests/no-bom.ts
@@ -0,0 +1,33 @@
+import * as fs from 'fs';
+
+import { getRuleName } from 'sonarwhal/dist/src/lib/utils/rule-helpers';
+import { RuleTest } from 'sonarwhal/dist/tests/helpers/rule-test-type';
+import * as ruleRunner from 'sonarwhal/dist/tests/helpers/rule-runner';
+
+const ruleName = getRuleName(__dirname);
+const bom = fs.readFileSync(`${__dirname}/fixtures/bom.html`); // eslint-disable-line no-sync
+const noBom = fs.readFileSync(`${__dirname}/fixtures/no-bom.html`); // eslint-disable-line no-sync
+
+const tests: Array<RuleTest> = [
+    {
+        name: 'HTML with no BOM should pass',
+        serverConfig: {
+            '/': {
+                content: noBom,
+                headers: { 'Content-Type': 'text/html' }
+            }
+        }
+    },
+    {
+        name: `HTML with BOM should fail`,
+        reports: [{ message: `Text based files shouldn't start with the BOM character to force UTF-8 encoding` }],
+        serverConfig: {
+            '/': {
+                content: bom,
+                headers: { 'Content-Type': 'text/html' }
+            }
+        }
+    }
+];
+
+ruleRunner.testRule(ruleName, tests);
