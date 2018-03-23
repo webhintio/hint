9cf6ebc9a8077873ed9e31fada7b25698eef0abc
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2018-03-23 09:43:54 -0700

    Chore: Improve coverage for `rule-babel-config`

--- packages/rule-babel-config/tests/tests.ts
+++ packages/rule-babel-config/tests/tests.ts
@@ -16,6 +16,11 @@ const tests: Array<RuleLocalTest> = [
         reports: [{ message: `'moduleId' should be string.` }]
     },
     {
+        name: `Invalid json file should fail`,
+        path: path.join(__dirname, 'fixtures', 'invalid-json', 'package.json'),
+        reports: [{ message: `Unexpected token ' in JSON at position 12` }]
+    },
+    {
         name: `If package.json doesn't contain "babel" property, it should pass`,
         path: path.join(__dirname, 'fixtures', 'no-babel-package-json', 'package.json')
     },
@@ -27,6 +32,16 @@ const tests: Array<RuleLocalTest> = [
         name: `If package.json contain invalid "babel" property, it should fail`,
         path: path.join(__dirname, 'fixtures', 'has-invalid-babel-package-json', 'package.json'),
         reports: [{ message: `'moduleId' should be string.` }]
+    },
+    {
+        name: `If .babelrc contain an additional property, it should fail`,
+        path: path.join(__dirname, 'fixtures', 'has-additional-property', '.babelrc'),
+        reports: [{ message: `'' should NOT have additional properties. Additional property found 'additional'.` }]
+    },
+    {
+        name: `If .babelrc contain an invalid value, it should fail`,
+        path: path.join(__dirname, 'fixtures', 'has-invalid-enum-property', '.babelrc'),
+        reports: [{ message: `'sourceMaps' should be equal to one of the allowed values 'both, inline, true, false'. Value found 'invalidValue'` }]
     }
 ];
 
