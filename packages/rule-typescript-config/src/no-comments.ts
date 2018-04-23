f240e3104dd46deb153cf444144e7b7f153dd222
molant <amolleda@gmail.com>
2018-04-23 13:21:35 -0700

    Chore: Refactor how to check config values

--- packages/rule-typescript-config/src/no-comments.ts
+++ packages/rule-typescript-config/src/no-comments.ts
@@ -7,8 +7,7 @@ import { RuleScope } from 'sonarwhal/dist/src/lib/enums/rulescope';
 import { RuleContext } from 'sonarwhal/dist/src/lib/rule-context';
 import { IRule, RuleMetadata } from 'sonarwhal/dist/src/lib/types';
 import { debug as d } from 'sonarwhal/dist/src/lib/utils/debug';
-
-import { TypeScriptConfigParse } from '@sonarwhal/parser-typescript-config/dist/src/types';
+import { configChecker } from './helpers/config-checker';
 
 const debug: debug.IDebugger = d(__filename);
 
@@ -30,14 +29,7 @@ export default class TypeScriptConfigNoComments implements IRule {
     }
 
     public constructor(context: RuleContext) {
-
-        const validate = async (evt: TypeScriptConfigParse) => {
-            const { config, resource } = evt;
-
-            if (!(config && config.compilerOptions && config.compilerOptions.removeComments)) {
-                await context.report(resource, null, 'The compiler option "removeComments" should be enabled to reduce the output size.');
-            }
-        };
+        const validate = configChecker('compilerOptions.removeComments', true, 'The compiler option "removeComments" should be enabled to reduce the output size.', context);
 
         context.on('parse::typescript-config::end', validate);
     }
--- packages/rule-typescript-config/src/strict.ts
+++ packages/rule-typescript-config/src/strict.ts
@@ -7,8 +7,7 @@ import { RuleScope } from 'sonarwhal/dist/src/lib/enums/rulescope';
 import { RuleContext } from 'sonarwhal/dist/src/lib/rule-context';
 import { IRule, RuleMetadata } from 'sonarwhal/dist/src/lib/types';
 import { debug as d } from 'sonarwhal/dist/src/lib/utils/debug';
-
-import { TypeScriptConfigParse } from '@sonarwhal/parser-typescript-config/dist/src/types';
+import { configChecker } from './helpers/config-checker';
 
 const debug: debug.IDebugger = d(__filename);
 
@@ -30,14 +29,7 @@ export default class TypeScriptConfigStrict implements IRule {
     }
 
     public constructor(context: RuleContext) {
-
-        const validate = async (evt: TypeScriptConfigParse) => {
-            const { config, resource } = evt;
-
-            if (!config.compilerOptions.strict) {
-                await context.report(resource, null, 'The compiler option "strict" should be enabled to reduce type errors.');
-            }
-        };
+        const validate = configChecker('compilerOptions.strict', true, 'The compiler option "strict" should be enabled to reduce type errors.', context);
 
         context.on('parse::typescript-config::end', validate);
     }
