7f3891d7fe17c282971ab9ed210649c0165579ca
molant <amolleda@gmail.com>
2018-04-24 10:35:44 -0700

    Docs: Update examples and typos

--- packages/rule-typescript-config/src/no-comments.ts
+++ packages/rule-typescript-config/src/no-comments.ts
@@ -1,6 +1,6 @@
 /**
- * @fileoverview `typescript-config/no-comments` checks that the property `removeComments`
- * is enabled in your TypeScript configuration file (i.e `tsconfig.json`).
+ * @fileoverview `typescript-config/no-comments` checks if the property `removeComments`
+ * is enabled in the TypeScript configuration file (i.e `tsconfig.json`).
  */
 import { Category } from 'sonarwhal/dist/src/lib/enums/category';
 import { RuleScope } from 'sonarwhal/dist/src/lib/enums/rulescope';
@@ -21,7 +21,7 @@ export default class TypeScriptConfigNoComments implements IRule {
     public static readonly meta: RuleMetadata = {
         docs: {
             category: Category.development,
-            description: '`typescript-config/no-comments` checks that the property `removeComments` is enabled in your TypeScript configuration file (i.e `tsconfig.json`)'
+            description: '`typescript-config/no-comments` checks that the property `removeComments` is enabled in the TypeScript configuration file (i.e `tsconfig.json`).'
         },
         id: 'typescript-config/no-comments',
         schema: [],
