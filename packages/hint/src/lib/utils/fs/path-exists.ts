f8b681d89bb0bf80135847f94bf13bd96ccc80c7
molant <amolleda@gmail.com>
2018-07-17 16:34:09 -0700

    Fix: Remove `shelljs` usage

--- packages/hint/src/lib/utils/fs/path-exists.ts
+++ packages/hint/src/lib/utils/fs/path-exists.ts
@@ -1,6 +1,12 @@
-import { test } from 'shelljs';
+import { statSync } from 'fs';
 
 /** Check if a path exists */
 export default (pathString: string): boolean => {
-    return test('-e', pathString);
+    try {
+        const stats = statSync(pathString);
+
+        return stats.isDirectory();
+    } catch (e) {
+        return false;
+    }
 };
