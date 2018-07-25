b431ab48ad6022f0a2ab108b993eb160c1fe81dc
Tony Ross <antross@gmail.com>
2018-07-25 16:04:09 -0500

    New: Add CSS parser
    Add CSS parser built on top of PostCSS. Currently it only
    supports CSS, but support for LESS and SASS can be added in
    the future using existing PostCSS plugins.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Fix #1115
    Close #1194

---
+++ packages/parser-css/README.md
@@ -0,0 +1,46 @@
+# CSS parser (`@hint/parser-css`)
+
+The `CSS` parser is built on top of [PostCSS][postcss] so hints can
+analyze `CSS` files.
+
+To use it you will have to install it via `npm`:
+
+```bash
+npm install @hint/parser-css
+```
+
+Note: You can make `npm` install it as a `devDependency` using the
+`--save-dev` parameter, or to install it globally, you can use the
+`-g` parameter. For other options see [`npm`'s
+documentation](https://docs.npmjs.com/cli/install).
+
+And then activate it via the [`.hintrc`][hintrc] configuration file:
+
+```json
+{
+    "connector": {...},
+    "formatters": [...],
+    "hints": {
+        ...
+    },
+    "parsers": ["css"],
+    ...
+}
+```
+
+## Events emitted
+
+This `parser` emits the event `parse::css` of type `StyleParse`
+which has the following information:
+
+* `ast`: a PostCSS `Root` object containing the AST.
+  See the [PostCSS `walk*` APIs][postcss-walk] for help navigating the AST.
+* `code`: a string containing the raw stylesheet source code.
+* `resource`: the parsed resource. If the CSS is in a `style tag`
+  and not a file, the value will be `Inline CSS`.
+
+<!-- Link labels: -->
+
+[hintrc]: https://webhint.io/docs/user-guide/further-configuration/hintrc-formats/
+[postcss]: https://postcss.org/
+[postcss-walk]: https://api.postcss.org/Container.html#walk
---
+++ packages/parser-css/src/parser.ts
@@ -0,0 +1,76 @@
+import * as postcss from 'postcss';
+
+import * as logger from 'hint/dist/src/lib/utils/logging';
+import normalizeString from 'hint/dist/src/lib/utils/misc/normalize-string';
+import { IAsyncHTMLElement, ElementFound, FetchEnd, Parser } from 'hint/dist/src/lib/types';
+import { StyleParse } from './types';
+import { Engine } from 'hint/dist/src/lib/engine';
+
+const styleContentRegex: RegExp = /^<style[^>]*>([\s\S]*)<\/style\s*>$/;
+
+export default class CSSParser extends Parser {
+    public constructor(engine: Engine) {
+        super(engine, 'css');
+
+        engine.on('fetch::end::css', this.parseCSS.bind(this));
+        engine.on('element::style', this.parseStyleTag.bind(this));
+    }
+
+    private async emitCSS(code: string, resource: string) {
+
+        try {
+            const ast = postcss.parse(code, { from: resource });
+
+            const styleData: StyleParse = {
+                ast,
+                code,
+                resource
+            };
+
+            await this.engine.emitAsync(`parse::${this.name}::end`, styleData);
+
+        } catch (err) {
+            logger.error(`Error parsing code: ${code} - ${err}`);
+        }
+    }
+
+    private async parseCSS(fetchEnd: FetchEnd) {
+        const code = fetchEnd.response.body.content;
+        const resource = fetchEnd.resource;
+
+        await this.emitCSS(code, resource);
+    }
+
+    private isCSSType(element: IAsyncHTMLElement) {
+        const type = normalizeString(element.getAttribute('type'));
+
+        /*
+         * From: https://html.spec.whatwg.org/multipage/semantics.html#update-a-style-block
+         *
+         * If element's type attribute is present and its value is neither
+         * the empty string nor an ASCII case-insensitive match for
+         * "text/css", then return.
+         */
+        return !type || type === 'text/css';
+    }
+
+    private getStyleContent(styleTagText) {
+        const match = styleTagText.match(styleContentRegex);
+
+        return match[1].trim();
+    }
+
+    private async parseStyleTag(elementFound: ElementFound) {
+        const element: IAsyncHTMLElement = elementFound.element;
+
+        if (!this.isCSSType(element)) {
+            // Ignore if it is not CSS.
+            return;
+        }
+
+        const code = this.getStyleContent(await element.outerHTML());
+        const resource: string = 'Inline CSS';
+
+        await this.emitCSS(code, resource);
+    }
+}
