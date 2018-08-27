8c5ee7a1866ab6a217ba12634ac77946c491fdf8
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2018-08-27 15:09:48 -0700

    Fix: Update `jsdom` to `v12.1.0`
    * Update to latest `jsdom` version.
    * Enable `includeNodeLocations` to jsdom.
    * Improve `@types/jsdom` with ResourceLoader.
    * Add polyfills to increase compatibility.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Fix #163
    Fix #1289
    Fix #1223

--- packages/connector-jsdom/src/evaluate-runner.ts
+++ packages/connector-jsdom/src/evaluate-runner.ts
@@ -1,9 +1,15 @@
 import * as vm from 'vm';
 
-import * as jsdom from 'jsdom/lib/old-api';
+import { JSDOM, VirtualConsole } from 'jsdom';
 import * as jsdomutils from 'jsdom/lib/jsdom/living/generated/utils';
 
-const run = (data) => {
+import { debug as d } from 'hint/dist/src/lib/utils/debug';
+
+import { beforeParse } from './before-parse';
+
+const debug: debug.IDebugger = d(__filename);
+
+const run = async (data) => {
     const { source } = data;
     const result = {
         error: null,
@@ -12,39 +18,52 @@ const run = (data) => {
     const url = process.argv[2];
     const waitFor = parseInt(process.argv[3], 10);
 
-    jsdom.env({
-        done: (error, window) => {
-            if (error) {
-                result.error = error;
+    const virtualConsole = new VirtualConsole();
 
-                return process.send(result);
+    virtualConsole.on('error', (err) => {
+        debug(err);
+    });
+
+    virtualConsole.on('jsdomError', (err) => {
+        debug(err);
+    });
+
+    const jsdom = await JSDOM.fromURL(url, {
+        beforeParse: beforeParse(url),
+        pretendToBeVisual: true,
+        resources: 'usable',
+        runScripts: 'dangerously',
+        virtualConsole
+    });
+
+    const onLoad = () => {
+        /*
+         * Even though `onLoad()` is called on `window.onload`
+         * (so all resoruces and scripts executed), we might want
+         * to wait a few seconds if the site is lazy loading something.
+         */
+        return setTimeout(async () => {
+            try {
+                const script: vm.Script = new vm.Script(source);
+                const evaluteResult = await script.runInContext(jsdomutils.implForWrapper(jsdom.window.document)._global);
+
+                result.evaluate = evaluteResult;
+            } catch (err) {
+                result.error = err;
             }
 
-            /*
-             * Even though `done()` is called after `window.onload`
-             * (so all resoruces and scripts executed), we might want
-             * to wait a few seconds if the site is lazy loading something.
-             */
-            return setTimeout(async () => {
-                try {
-                    const script: vm.Script = new vm.Script(source);
-                    const evaluteResult = await script.runInContext(jsdomutils.implForWrapper(window.document)._global);
+            process.send(result);
+        }, waitFor);
+    };
 
-                    result.evaluate = evaluteResult;
-                } catch (err) {
-                    result.error = err;
-                }
+    const onError = (error) => {
+        result.error = error;
 
-                process.send(result);
-            }, waitFor);
-        },
-        features: {
-            FetchExternalResources: ['script', 'link', 'img'],
-            ProcessExternalResources: ['script'],
-            SkipExternalResources: false
-        },
-        url
-    });
+        process.send(result);
+    };
+
+    jsdom.window.addEventListener('load', onLoad, { once: true });
+    jsdom.window.addEventListener('error', onError);
 };
 
 process.on('message', run);
