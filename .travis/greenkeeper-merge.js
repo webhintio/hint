75cc9106cf5da6b15b976bad04c901391451a9aa
molant <amolleda@gmail.com>
2018-09-25 11:33:04 -0700

    Chore: Auto-merge Greenkeeper PRs
    * Update commit message from Greenekeeper.
    * Squash commits for same dependency update into one.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Fix #938
    Close #1340

--- .travis/greenkeeper-merge.js
+++ .travis/greenkeeper-merge.js
@@ -6,64 +6,53 @@ const { exec, getLastCommitAuthorEmail, getLastCommitAuthorName, getLastCommitSu
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-const generateNewCommitMessage = (originalCommitMessage) => {
-    const regex = /.*update\s(.*)\sto version\s([\w.-]+)/gi;
-    const result = regex.exec(originalCommitMessage);
-
-    if (result) {
-        return `Chore: \\\`${result[1]}\\\` to \\\`v${result[2]}\\\``;
-    }
-
-    return null;
+// The format of the log should be `--oneline`
+const getSHAs = (log) => {
+    return log.trim()
+        .split('\n')
+        .map((line) => {
+            return line.trim().split(' ')[0];
+        });
 };
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 const main = async () => {
     const GIT_COMMIT_SUBJECT = await getLastCommitSubject();
     const COMMITS_TO_SQUASH = await getLastCommitsWithSameMessage();
-    const NEW_COMMIT_MESSAGE = generateNewCommitMessage(GIT_COMMIT_SUBJECT);
-
     const GIT_GREENKEEPER_BRANCH = process.env.TRAVIS_BRANCH; // eslint-disable-line no-process-env
     const GIT_REPO_SLUG = process.env.TRAVIS_REPO_SLUG; // eslint-disable-line no-process-env
     const GIT_USER_EMAIL = await getLastCommitAuthorEmail();
     const GIT_USER_NAME = await getLastCommitAuthorName();
 
-    /*
-     * Ensure that things work as expected for shallow clones.
-     * (This is usually the case with CIs such as Travis)
-     */
-    await exec('Set remote.', `git remote set-branches origin ${GIT_GREENKEEPER_BRANCH}`);
+    await exec('Configure the Git user name.', `git config --global user.name "${GIT_USER_NAME}"`);
+    await exec('Configure the Git user email.', `git config --global user.email "${GIT_USER_EMAIL}"`);
 
-    // Update local data.
-    await exec('Fetch remote.', `git fetch`);
-
-    await exec('Configure the Git user name and email.',
-        `git config --global user.name "${GIT_USER_NAME}"
-        git config --global user.email "${GIT_USER_EMAIL}"`);
+    await exec(`Set remote.`, `git remote set-branches origin master`);
+    await exec('Fetch content from origin', 'git fetch origin --unshallow');
 
     /*
-     * `install` step in Travis does a `yarn`. If versions
-     * are different, yarn.lock is different so discard changes.
+     * Greenkeeper updates each package in the monorepo with the same
+     * dependency in different commits but the project style is only 1.
+     *
+     * Instead of rebase/squash, do a soft reset with the same commit
+     * message.
      */
-    await exec(`Discard 'yarn.lock'.`, `git checkout yarn.lock`);
+    await exec(`Soft reset latest ${COMMITS_TO_SQUASH} commits.`, `git reset --soft HEAD~${COMMITS_TO_SQUASH}`);
+    const files = await exec(`Files changed.`, `git status`);
+
+    console.log(files);
+
+    await exec(`Create new commit.`, `git commit --message="${GIT_COMMIT_SUBJECT}"`);
 
     /*
-     * Greenkeeper updates each package in the monorepo with
-     * the same dependency in different commits: squash and
-     * update message.
+     * The changes are in dettached mode, and couldn't find a way to
+     * rebase correctly in master. Store the SHA to cherry-pick later
      */
-    await exec('Commit changes and squash.', `git reset --soft HEAD~${COMMITS_TO_SQUASH} && git commit --message="${NEW_COMMIT_MESSAGE}"`);
+    const log = await exec(`Latest log entry.`, `git log --oneline -n 1`);
+    const latestSha = getSHAs(log)[0];
 
-    await exec(`Update 'origin' remote.`, `git remote remove origin && git remote add origin git@github.com:${GIT_REPO_SLUG}.git`);
-    await exec(`Fetch content from 'origin'.`, 'git fetch origin');
     await exec(`Switch to the 'master' branch.`, 'git checkout master');
+    await exec(`Cherry-pick '${latestSha}' into 'master'.`, `git cherry-pick ${latestSha}`);
 
-    const changes = await exec('List changed files.', 'git status');
-
-    console.log(changes);
-
-    await exec(`Rebase '${GIT_GREENKEEPER_BRANCH}' branch into 'master'.`, `git rebase ${GIT_GREENKEEPER_BRANCH}`);
     await exec(`Push changes to 'master'.`, `git push git@github.com:${GIT_REPO_SLUG}.git master`);
     await exec(`Delete '${GIT_GREENKEEPER_BRANCH}' branch.`, `git push git@github.com:${GIT_REPO_SLUG}.git :${GIT_GREENKEEPER_BRANCH}`);
 };
