ee70b491b0a502e4494e4a92192b578e9068e7e4
Quique Fdez Guerra <quique@ckgrafico.com>
2018-10-15 15:38:41 +0200

    Chore: Check if doctype is the only info in the first line

--- packages/hint-valid-doctype/src/hint.ts
+++ packages/hint-valid-doctype/src/hint.ts
@@ -4,7 +4,7 @@
 import { Category } from 'hint/dist/src/lib/enums/category';
 import { HintScope } from 'hint/dist/src/lib/enums/hintscope';
 import { HintContext } from 'hint/dist/src/lib/hint-context';
-import { IHint, HintMetadata, FetchEnd } from 'hint/dist/src/lib/types';
+import { IHint, HintMetadata, FetchEnd, IAsyncHTMLElement, ResponseBody } from 'hint/dist/src/lib/types';
 import { debug as d } from 'hint/dist/src/lib/utils/debug';
 
 const debug: debug.IDebugger = d(__filename);
@@ -28,10 +28,37 @@ export default class implements IHint {
     }
 
     public constructor(context: HintContext) {
+        const validDoctype = '<!doctype html>';
+        const doctypeRegex = /(<!doctype ).+(>).+/;
+
+        const checkDoctypeFirstLine = async (resource: string, element: IAsyncHTMLElement, content: string) => {
+            debug(`Checking if the doctype is in the first line, and there is nothing else.`);
+
+            const firstLine = content.split(/\r|\n/)[0];
+            const matched = firstLine.match(doctypeRegex);
+
+            if (!matched || matched.length < 1) {
+                await context.report(resource, element, `The first line does not contain a doctype.`);
+                return;
+            }
+
+            const [contentDoctype] = matched;
+
+            if (contentDoctype !== validDoctype) {
+                await context.report(resource, element, `The first line contain more than a valid doctype ${contentDoctype}`);
+            }
+        };
 
         const onFetchEndHTML = async (fetchEnd: FetchEnd) => {
-            debug(`Validating doctype`);
-            console.log(fetchEnd);
+            const { resource, element, response } = fetchEnd;
+
+            if (!response || !response.body || !response.body.content) {
+                await context.report(resource, element, 'Content has no body.');
+            }
+
+            const { body } = response;
+            const { content } = body;
+            await checkDoctypeFirstLine(resource, element, content);
         };
 
         context.on('fetch::end::html', onFetchEndHTML);
