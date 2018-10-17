7eb6954600f670cc2ff394de1e1bef004c09ea19
Quique Fdez Guerra <quique@ckgrafico.com>
2018-10-17 16:20:41 +0200

    Chore: Add info

--- packages/hint-compat-api/src/compat-api-css.ts
+++ packages/hint-compat-api/src/compat-api-css.ts
@@ -1,13 +1,13 @@
 /**
- * @fileoverview desc
+ * @fileoverview Hint to validate if the doctype is correct
  */
 
 import { Category } from 'hint/dist/src/lib/enums/category';
-import { HintContext } from 'hint/dist/src/lib/hint-context';
-// The list of types depends on the events you want to capture.
-import { IHint, FetchStart, FetchEnd, HintMetadata } from 'hint/dist/src/lib/types';
-import { debug as d } from 'hint/dist/src/lib/utils/debug';
 import { HintScope } from 'hint/dist/src/lib/enums/hintscope';
+import { HintContext } from 'hint/dist/src/lib/hint-context';
+import { IHint, HintMetadata, IAsyncHTMLElement } from 'hint/dist/src/lib/types';
+import { CSSParser } from '@hint/parser-css';
+import { debug as d } from 'hint/dist/src/lib/utils/debug';
 
 const debug: debug.IDebugger = d(__filename);
 
@@ -17,70 +17,29 @@ const debug: debug.IDebugger = d(__filename);
  * ------------------------------------------------------------------------------
  */
 
-export default class CompatApiCompatApiCssHint implements IHint {
+export default class implements IHint {
 
     public static readonly meta: HintMetadata = {
         docs: {
-            category: Category.development,
-            description: `desc`
+            category: Category.interoperability,
+            description: `Hint to validate if the doctype is correct`
         },
-        id: 'compat-api/compat-api-css',
-        schema: [
-            /*
-             * If you want to allow the user to configure your hint
-             * you should use a valid JSON schema. More info in:
-             * https://webhint.io/docs/contributor-guide/hints/#themetaproperty
-             */
-        ],
+        id: 'doctype',
+        schema: [],
         scope: HintScope.any
     }
 
     public constructor(context: HintContext) {
 
-        // Your code here.
-        const validateFetchStart = async (fetchStart: FetchStart) => {
-            // Code to validate the hint on the event fetch::start.
-
-            const { resource } = fetchStart;
-            
-            debug(`Validating hint compat-api-css`);
-            
-            /*
-             * This is where all the magic happens. Any errors found should be
-             * reported using the `context` object. E.g.:
-             * await context.report(resource, null, 'Add error message here.');
-             *
-             * More information on how to develop a hint is available in:
-             * https://webhint.io/docs/contributor-guide/hints/
-             */
-            
-            if (Math.ceil(Math.random()) === 0) {
-                await context.report(resource, null, 'Add error message here.');
-            }
-        };
-        const validateFetchEnd = async (fetchEnd: FetchEnd) => {
-            // Code to validate the hint on the event fetch::end::*.
-
-            const { resource } = fetchEnd;
-            
-            debug(`Validating hint compat-api-css`);
-            
-            /*
-             * This is where all the magic happens. Any errors found should be
-             * reported using the `context` object. E.g.:
-             * await context.report(resource, null, 'Add error message here.');
-             *
-             * More information on how to develop a hint is available in:
-             * https://webhint.io/docs/contributor-guide/hints/
-             */
-            
-            if (Math.ceil(Math.random()) === 0) {
-                await context.report(resource, null, 'Add error message here.');
-            }
+        const checkNoDuplicateDoctype = async (resource: string, element: IAsyncHTMLElement | null, content: string): Promise<void> => {
+            debug(`Checking that there is only one doctype tag in the document`);
+            await context.report(resource, element, `There is more than one doctype tag in the document`);
         };
 
-        context.on('fetch::end::*', validateFetchEnd);
-        context.on('fetch::start', validateFetchStart);
-        // As many events as you need
+        const onParseCSS = async (cssParser: CSSParser): Promise<void> => {
+            debugger
+        };
+
+        context.on('parse::css::end', onParseCSS);
     }
 }
