04f21c7d68cf15b3e26c32c0b53f6089f0df32fe
PLAINCONCEPTS\ichen <ichen@plainconcepts.com>
2018-10-23 13:33:16 +0200

    Chore: Added list of getter for all browserlist browsers

---
+++ packages/hint-compat-api/src/services/user-browsers-config.ts
@@ -0,0 +1,104 @@
+const getUserBrowsers = ( targetedBrowsers: string[] ) => {
+    
+    //Android WebView
+    const androidWebView = () => {
+
+    }
+
+    //Baidu Broswer
+    const baidu = () => {
+
+    }
+
+    //Blackberry browser
+    const blackBerry = () => {
+
+    }
+
+    //Google Chrome
+    const chrome = () => {
+
+    }
+
+    //Chrome for Android
+    const chromeAndroid = () => {
+
+    }
+
+    //MS Edge
+    const edge = () => {
+
+    }
+
+    //Electron framework
+    const electron = () => {
+
+    }
+
+    //Internet Explorer
+    const explorer = () => {
+        //Explorer or ie
+    }
+
+    //Explorer Mobile
+    const explorerMobile = () => {
+        //ExplorerMobile or ie_mob
+    }
+
+    //Mozilla Firefox
+    const firefox = () => {
+        //Firefox or ff
+    }
+
+    //Firefox for Android
+    const firefoxAndroid = () => {
+        //FirefoxAndroid or and_ff
+    }
+
+    //iOS Safari
+    const iOS = () => {
+        //iOS or ios_saf 
+    }
+
+    //Node.js
+    const node = () => {
+        //Node
+    }
+
+    //Opera
+    const opera = () => {
+        //Opera
+    }
+
+    //Opera Mini
+    const operaMini = () => {
+        //OperaMini or op_mini
+    }
+
+    //Opera Mobile
+    const operaMobile = () => {
+        //OperaMobile or op_mob
+    }
+
+    //QQ Browser for Android
+    const qqAndroid = () => {
+        //QQAndroid or and_qq for 
+    }
+
+    //Desktop Safari
+    const safari = () => {
+        //Safari
+    }
+
+    //Samsung Internet
+    const samsung = () => {
+        //Samsung
+    }
+    
+    //UC Browser for Android
+    const ucAndroid = () => {
+        //UCAndroid or and_uc
+    }
+}
+
+export { getUserBrowsers }
