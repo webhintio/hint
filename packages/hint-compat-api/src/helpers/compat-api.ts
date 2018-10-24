cca437e489d1815004be93140cd6a9078011480e
Quique Fdez Guerra <quique@ckgrafico.com>
2018-10-24 11:47:20 +0200

    Fix: Linting

--- packages/hint-compat-api/src/helpers/compat-api.ts
+++ packages/hint-compat-api/src/helpers/compat-api.ts
@@ -1,21 +1,28 @@
 // Waiting for this PR https://github.com/mdn/browser-compat-data/pull/3004
 const bcd = require('mdn-browser-compat-data');
+
 import { forEach } from 'lodash';
+
 type CompatNamespace = 'css' | 'javascript' | 'html';
 
 export class CompatApi {
     private compatData: any; // Any because no types by the moment
+    private browsers: any;
 
-    constructor(namespaceName: CompatNamespace, private browsers: any /* Isabella PR */) {
+    public constructor(namespaceName: CompatNamespace, browsers: any /* Isabella PR */) {
+        this.browsers = browsers;
         this.compatData = bcd[namespaceName];
         this.compatData = this.applyBrowsersConfiguration();
     }
 
     private applyBrowsersConfiguration(): any {
         const compatData = {} as any;
+
         forEach(this.compatData, (apiTypeValue, apiTypeKey) => {
             const apiType = {} as any;
+
             forEach(apiTypeValue, (propertyValue, propertyKey) => {
+
                 if (!this.apiPropertyIsRequired(propertyValue)) {
                     return;
                 }
@@ -30,8 +37,8 @@ export class CompatApi {
 
     private apiPropertyIsRequired(propertyValue: any /* waiting types */): boolean {
         // TODO: Here we are checking only parent but this object has children
-
         let isRequired = true;
+
         forEach(this.browsers, (values, browser) => {
             if (!isRequired || !propertyValue.__compat || !propertyValue.__compat.support) {
                 return;
@@ -41,13 +48,15 @@ export class CompatApi {
 
             if (!propertySupport) {
                 isRequired = false;
+
                 return;
             }
 
             const version = propertySupport.version_added;
-            isRequired = !!version
-                && version === true
-                || !isNaN(parseFloat(version)) && (version >= values.min && version <= (values.max || Infinity));
+
+            isRequired = !!version &&
+                version === true ||
+                !isNaN(parseFloat(version)) && (version >= values.min && version <= (values.max || Infinity));
         });
 
         return isRequired;
