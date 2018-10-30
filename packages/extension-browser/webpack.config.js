188f22cada5bd2e2708968bb37055af4598edde5
Tony Ross <antross@gmail.com>
2018-10-30 13:34:38 -0500

    Chore: Simplify webpack configuration

--- packages/extension-browser/webpack.config.js
+++ packages/extension-browser/webpack.config.js
@@ -1,27 +1,13 @@
 const path = require('path');
 
-const baseConfig = {
+module.exports = {
+    entry: {
+        'background-script': './dist/src/background-script.js',
+        'content-script/webhint': './dist/src/content-script/webhint.js',
+        'devtools/devtools': './dist/src/devtools/devtools.js',
+        'devtools/panel/panel': './dist/src/devtools/panel/panel.js'
+    },
     mode: 'none',
-    node: { fs: 'empty' },
-    resolve: {
-        alias: { // eslint-disable-line
-            url$: path.resolve(__dirname, 'dist/src/shims/url.js')
-        }
-    }
-};
-
-const backgroundConfig = {
-    ...baseConfig,
-    entry: './dist/src/background-script.js',
-    output: {
-        filename: 'background-script.js',
-        path: path.resolve(__dirname, 'dist/bundle')
-    }
-};
-
-const contentConfig = {
-    ...baseConfig,
-    entry: './dist/src/content-script/webhint.js',
     module: {
         rules: [
             {
@@ -30,28 +16,14 @@ const contentConfig = {
             }
         ]
     },
+    node: { fs: 'empty' },
     output: {
-        filename: 'webhint.js',
-        path: path.resolve(__dirname, 'dist/bundle/content-script')
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist/bundle')
+    },
+    resolve: {
+        alias: { // eslint-disable-line
+            url$: path.resolve(__dirname, 'dist/src/shims/url.js')
+        }
     }
 };
-
-const devtoolsConfig = {
-    ...baseConfig,
-    entry: './dist/src/devtools/devtools.js',
-    output: {
-        filename: 'devtools.js',
-        path: path.resolve(__dirname, 'dist/bundle/devtools')
-    }
-};
-
-const webhintPanelConfig = {
-    ...baseConfig,
-    entry: './dist/src/devtools/panel/panel.js',
-    output: {
-        filename: 'panel.js',
-        path: path.resolve(__dirname, 'dist/bundle/devtools/panel')
-    }
-};
-
-module.exports = [backgroundConfig, contentConfig, devtoolsConfig, webhintPanelConfig];
