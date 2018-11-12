3576075caea16cc64b7d18a89b8e563c4a55548e
PLAINCONCEPTS\ichen <ichen@plainconcepts.com>
2018-11-12 18:25:43 +0100

    Chore: Added tests

--- packages/hint-compat-api/tests/compat-api-css-next.ts
+++ packages/hint-compat-api/tests/compat-api-css-next.ts
@@ -26,23 +26,23 @@ const generateCSSConfig = (fileName: string) => {
  * https://webhint.io/docs/contributor-guide/how-to/test-hints/
  */
 
-// const featureAddedBeforeTargetedBrowsers: Array<HintTest> = [
-//     {
-//         name: 'Features that were added in versions earlier than the targeted browsers should pass.',
-//         serverConfig: generateCSSConfig('charset')
-//     }
-// ];
+const featureAddedBeforeTargetedBrowsers: Array<HintTest> = [
+    {
+        name: 'Features that were added in versions earlier than the targeted browsers should pass.',
+        serverConfig: generateCSSConfig('charset')
+    }
+];
 
-// hintRunner.testHint(hintPath, featureAddedBeforeTargetedBrowsers, { browserslist: ['last 2 Chrome versions'], parsers: ['css']});
+hintRunner.testHint(hintPath, featureAddedBeforeTargetedBrowsers, { browserslist: ['last 2 Chrome versions'], parsers: ['css']});
 
-// const prefixedFeatureAddedTrue: Array<HintTest> = [
-//     {
-//         name: 'Prefixed features that have version added as true should pass.',
-//         serverConfig: generateCSSConfig('box-flex-prefix')
-//     }
-// ];
+const prefixedFeatureAddedTrue: Array<HintTest> = [
+    {
+        name: 'Prefixed features that have version added as true should pass.',
+        serverConfig: generateCSSConfig('box-flex-prefix')
+    }
+];
 
-// hintRunner.testHint(hintPath, prefixedFeatureAddedTrue, { browserslist: ['chrome 32', 'chrome 63 - 65'], parsers: ['css']});
+hintRunner.testHint(hintPath, prefixedFeatureAddedTrue, { browserslist: ['chrome 32', 'chrome 63 - 65'], parsers: ['css']});
 
 const featureVersionAddedNull: Array<HintTest> = [
     {
@@ -53,23 +53,23 @@ const featureVersionAddedNull: Array<HintTest> = [
 
 hintRunner.testHint(hintPath, featureVersionAddedNull, { browserslist: ['and_chr 69'], parsers: ['css']});
 
-// const childFeatureVersionAddedNull: Array<HintTest> = [
-//     {
-//         name: 'Features using child properties that have version added as null under the targeted browsers should pass.',
-//         serverConfig: generateCSSConfig('text-transform')
-//     }
-// ];
+const childFeatureVersionAddedNull: Array<HintTest> = [
+    {
+        name: 'Features using child properties that have version added as null under the targeted browsers should pass.',
+        serverConfig: generateCSSConfig('text-transform')
+    }
+];
 
-// hintRunner.testHint(hintPath, childFeatureVersionAddedNull, { browserslist: ['chrome 65'], parsers: ['css']});
+hintRunner.testHint(hintPath, childFeatureVersionAddedNull, { browserslist: ['chrome 65'], parsers: ['css']});
 
-// const featureVersionAddedFalse: Array<HintTest> = [
-//     {
-//         name: 'Features that have version added as false should fail.',
-//         reports: [{ message: 'box-flex of CSS is not added on ie browser.' }],
-//         serverConfig: generateCSSConfig('box-flex')
-//     }
-// ];
+const featureVersionAddedFalse: Array<HintTest> = [
+    {
+        name: 'Features that have version added as false should fail.',
+        reports: [{ message: 'box-flex of CSS is not added on ie browser.' }],
+        serverConfig: generateCSSConfig('box-flex')
+    }
+];
 
-// hintRunner.testHint(hintPath, featureVersionAddedFalse, { browserslist: ['ie 11'], parsers: ['css']});
+hintRunner.testHint(hintPath, featureVersionAddedFalse, { browserslist: ['ie 11'], parsers: ['css']});
 
 
