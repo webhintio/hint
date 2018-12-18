e63084aae307dfc6e8f90c4eb9f1c0289b3737fc
PLAINCONCEPTS\ichen <ichen@plainconcepts.com>
2018-12-18 17:48:31 +0100

    Chore: Parser package json with basic tests

--- packages/parser-package-json/src/types.ts
+++ packages/parser-package-json/src/types.ts
@@ -6,14 +6,12 @@ export type PackageJsonInvalidJSON = ErrorEvent;
 /** Data type sent when the parse starts parsing */
 export type PackageJsonParseStart = Event;
 
-/** The object emitted by the `babel-config` parser */
+/** The object emitted by the `package-json` parser */
 export type PackageJsonParsed = Event & {
-    /** The babel config parsed */
+    /** The package json parsed */
     config: any;
     /** Find the location of a path within the original JSON source */
     getLocation: IJSONLocationFunction;
-    /** The original babel config */
-    originalConfig: any;
 };
 
 export type PackageJsonInvalidSchema = ErrorEvent & {
@@ -23,8 +21,6 @@ export type PackageJsonInvalidSchema = ErrorEvent & {
 
 export type PackageJsonEvents = Events & {
     'parse::end::package-json': PackageJsonParsed;
-    'parse::error::package-json::circular': ErrorEvent;
-    'parse::error::package-json::extends': ErrorEvent;
     'parse::error::package-json::json': PackageJsonInvalidJSON;
     'parse::error::package-json::schema': PackageJsonInvalidSchema;
     'parse::start::package-json': PackageJsonParseStart;
