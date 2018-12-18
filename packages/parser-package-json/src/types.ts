85a3d41b2178e8b28dbb433f8cd91864820e91a9
PLAINCONCEPTS\ichen <ichen@plainconcepts.com>
2018-12-18 12:14:02 +0100

    Chore: Pass basic case for package json parser

--- packages/parser-package-json/src/types.ts
+++ packages/parser-package-json/src/types.ts
@@ -1,59 +1,31 @@
 import { Event, ErrorEvent, Events } from 'hint/dist/src/lib/types/events';
 import { IJSONLocationFunction, ISchemaValidationError } from 'hint/dist/src/lib/types';
 
-export type BabelConfig = {
-    ast: boolean;
-    auxiliaryCommentAfter: string;
-    auxiliaryCommentBefore: string;
-    code: boolean;
-    comments: boolean;
-    compact: string;
-    env: object;
-    extends: string;
-    filename: string;
-    filenameRelative: string;
-    highlightCode: boolean;
-    ignore: string[] | string;
-    inputSourceMap: object;
-    keepModuleIdExtensions: boolean;
-    moduleId: string;
-    moduleIds: string;
-    moduleRoot: string;
-    only: string[] | string;
-    plugins: string | string[] | object[];
-    presets: string | string[] | object[];
-    retainLines: boolean;
-    sourceFileName: string;
-    sourceMaps: string | boolean;
-    sourceMapTarget: string;
-    sourceRoot: string;
-};
-
-export type BabelConfigInvalidJSON = ErrorEvent;
+export type PackageJsonInvalidJSON = ErrorEvent;
 
 /** Data type sent when the parse starts parsing */
-export type BabelConfigParseStart = Event;
+export type PackageJsonParseStart = Event;
 
 /** The object emitted by the `babel-config` parser */
-export type BabelConfigParsed = Event & {
+export type PackageJsonParsed = Event & {
     /** The babel config parsed */
-    config: BabelConfig;
+    config: any;
     /** Find the location of a path within the original JSON source */
     getLocation: IJSONLocationFunction;
     /** The original babel config */
-    originalConfig: BabelConfig;
+    originalConfig: any;
 };
 
-export type BabelConfigInvalidSchema = ErrorEvent & {
+export type PackageJsonInvalidSchema = ErrorEvent & {
     errors: ISchemaValidationError[];
     prettifiedErrors: string[];
 };
 
-export type BabelConfigEvents = Events & {
-    'parse::end::babel-config': BabelConfigParsed;
-    'parse::error::babel-config::circular': ErrorEvent;
-    'parse::error::babel-config::extends': ErrorEvent;
-    'parse::error::babel-config::json': BabelConfigInvalidJSON;
-    'parse::error::babel-config::schema': BabelConfigInvalidSchema;
-    'parse::start::babel-config': BabelConfigParseStart;
+export type PackageJsonEvents = Events & {
+    'parse::end::package-json': PackageJsonParsed;
+    'parse::error::package-json::circular': ErrorEvent;
+    'parse::error::package-json::extends': ErrorEvent;
+    'parse::error::package-json::json': PackageJsonInvalidJSON;
+    'parse::error::package-json::schema': PackageJsonInvalidSchema;
+    'parse::start::package-json': PackageJsonParseStart;
 };
