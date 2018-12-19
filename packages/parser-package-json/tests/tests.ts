f50192f39137357c8921b27210b84f2d88491fd7
PLAINCONCEPTS\ichen <ichen@plainconcepts.com>
2018-12-19 14:24:48 +0100

    Chore: Fixed linting and test messages

--- packages/parser-package-json/tests/tests.ts
+++ packages/parser-package-json/tests/tests.ts
@@ -16,7 +16,7 @@ const invalidJSON = `{
     "name": "app",
     "version":  "1.0.0",
     "scripts": [
-      "echo \"Error: no test specified\" && exit 1"
+      "echo "Error: no test specified" && exit 1"
     ],
     "dependencies": {
       "extend": "3.0.2"
@@ -24,7 +24,7 @@ const invalidJSON = `{
     "devDependencies": {
       "gulp": "^4.0.0"
     }
-  }`
+  }`;
 
 let sandbox: sinon.SinonSandbox;
 
@@ -45,74 +45,13 @@ test.afterEach((t) => {
     sandbox.restore();
 });
 
-test.serial('If the .json file being fetched is not package.json, the only event emitted should be fetch::end::json.', async (t) => {
-    sandbox.stub(path, 'basename').returns('foo.json');
-
-    await t.context.engine.emitAsync('fetch::end::json', {
-        resource: url.format(getAsUri(validPath)!),
-        response: { body: { content: JSON.stringify(validJSON) } }
-    });
-
-     t.true(t.context.engine.emitAsync.calledOnce);
-     t.is(t.context.engine.emitAsync.args[0][0], 'fetch::end::json');
-});
-
-test.serial('If we receive a valid json with a valid name, it should emit the event parse::end::package-json', async (t) => {
-    await t.context.engine.emitAsync('fetch::end::json', {
-        resource: url.format(getAsUri(validPath)!),
-        response: { body: { content: JSON.stringify(validJSON) } }
-    });
-
-    t.is(t.context.engine.emitAsync.args[2][0], 'parse::end::package-json');
-});
-
-test.serial(`Even if package.json contains an invalid schema, it should still emit the 'parse::start::package-json' event`, async (t) => {
-        await t.context.engine.emitAsync('fetch::end::json', {
-            resource: 'package.json',
-            response: { body: { content: invalidJSON } }
-        });
-    
-        t.is(t.context.engine.emitAsync.args[1][0], 'parse::start::package-json');
-});
-
-test.serial(`If package.json contains an invalid schema, it should emit the 'parse::error::package-json::schema' event`, async (t) => {
-    await t.context.engine.emitAsync('fetch::end::json', {
-        resource: 'package.json',
-        response: { body: { content: invalidJSON } }
-    });
-
-    t.is(t.context.engine.emitAsync.args[2][0], 'parse::error::package-json::schema');
-});
-
-test.serial(`If package.json contains an invalid schema, it should emit the an errors array containing the errors `, async (t) => {
-    await t.context.engine.emitAsync('fetch::end::json', {
-        resource: 'package.json',
-        response: { body: { content: invalidJSON } }
-    });
-
-    t.is(Object.keys(t.context.engine.emitAsync.args[2][1]).includes('errors'), true);
-    t.is(Array.isArray(t.context.engine.emitAsync.args[2][1].errors), true);
-    t.is(t.context.engine.emitAsync.args[2][1].errors.length, 1);
-});
-
-test.serial(`If package.json contains an invalid schema, it should emit a prettified version of the errors `, async (t) => {
-    await t.context.engine.emitAsync('fetch::end::json', {
-        resource: 'package.json',
-        response: { body: { content: invalidJSON } }
-    });
-
-    t.is(Object.keys(t.context.engine.emitAsync.args[2][1]).includes('prettifiedErrors'), true);
-    t.is(Array.isArray(t.context.engine.emitAsync.args[2][1].prettifiedErrors), true);
-    t.is(t.context.engine.emitAsync.args[2][1].prettifiedErrors.length, 1);
-});
-
 test.serial('If we receive a valid package.json, it should emit the event parse::end::package-json', async (t) => {
     await t.context.engine.emitAsync('fetch::end::json', {
         resource: url.format(getAsUri(validPath)!),
         response: { body: { content: JSON.stringify(validJSON) } }
     });
 
-    // 3 calls, fetch .json, parse start and parse end.
+    // 3 calls, fetch json, parse start and parse end.
     t.is(t.context.engine.emitAsync.callCount, 3);
     t.is(t.context.engine.emitAsync.args[1][0], 'parse::start::package-json');
     t.is(t.context.engine.emitAsync.args[2][0], 'parse::end::package-json');
@@ -127,15 +66,67 @@ test.serial('If we receive a valid package.json, it should emit exactly the cont
     t.deepEqual(t.context.engine.emitAsync.args[2][1].config, validJSON);
 });
 
-test.serial('If the file contains an invalid json, it should fail', async (t) => {
+test.serial('If the .json file being fetched is not package.json, the only event emitted should be fetch::end::json.', async (t) => {
+    sandbox.stub(path, 'basename').returns('foo.json');
+
+    await t.context.engine.emitAsync('fetch::end::json', {
+        resource: url.format(getAsUri(validPath)!),
+        response: { body: { content: JSON.stringify(validJSON) } }
+    });
+
+    t.true(t.context.engine.emitAsync.calledOnce);
+    t.is(t.context.engine.emitAsync.args[0][0], 'fetch::end::json');
+});
+
+test.serial(`Even if package.json contains an invalid schema, it should still emit the 'parse::start::package-json' event`, async (t) => {
+    await t.context.engine.emitAsync('fetch::end::json', {
+        resource: 'package.json',
+        response: { body: { content: invalidJSON } }
+    });
+
+    t.is(t.context.engine.emitAsync.args[1][0], 'parse::start::package-json');
+});
+
+test.serial(`If package.json contains an invalid schema, it should emit an error related to the schema of the package.json`, async (t) => {
+    await t.context.engine.emitAsync('fetch::end::json', {
+        resource: 'package.json',
+        response: { body: { content: invalidJSON } }
+    });
+
+    t.is(t.context.engine.emitAsync.args[2][0], 'parse::error::package-json::schema');
+});
+
+test.serial(`If package.json contains an invalid schema, it should emit an array containing the errors `, async (t) => {
+    await t.context.engine.emitAsync('fetch::end::json', {
+        resource: 'package.json',
+        response: { body: { content: invalidJSON } }
+    });
+
+    t.is(Object.keys(t.context.engine.emitAsync.args[2][1]).includes('errors'), true);
+    t.is(Array.isArray(t.context.engine.emitAsync.args[2][1].errors), true);
+    t.is(t.context.engine.emitAsync.args[2][1].errors.length, 1);
+});
+
+test.serial(`If package.json contains an invalid schema, it should emit an array of errors that is easily understandable `, async (t) => {
+    await t.context.engine.emitAsync('fetch::end::json', {
+        resource: 'package.json',
+        response: { body: { content: invalidJSON } }
+    });
+
+    t.is(Object.keys(t.context.engine.emitAsync.args[2][1]).includes('prettifiedErrors'), true);
+    t.is(Array.isArray(t.context.engine.emitAsync.args[2][1].prettifiedErrors), true);
+    t.is(t.context.engine.emitAsync.args[2][1].prettifiedErrors.length, 1);
+});
+
+test.serial(`If the file contains an invalid json, it should emit an error related to the json format of the package.json`, async (t) => {
     await t.context.engine.emitAsync('fetch::end::json', {
         resource: 'package.json',
         response: { body: { content: 'invalidJson' } }
     });
 
-    // 2 calls, fetch .json, and the error.
+    // 2 calls, fetch json, and the error.
     t.is(t.context.engine.emitAsync.callCount, 2);
-    t.is(t.context.engine.emitAsync.args[1][0], 'parse::error::package-json::json'); 
+    t.is(t.context.engine.emitAsync.args[1][0], 'parse::error::package-json::json');
 });
 
 test.serial(`If 'package.json' contains a non standard property, it should still emit the event parse::end::package-json`, async (t) => {
@@ -143,7 +134,7 @@ test.serial(`If 'package.json' contains a non standard property, it should still
         "name": "app",
         "foo":  "1.0.0",
         "scripts": {
-          "test": "echo \"Error: no test specified\" && exit 1",
+          "test": "echo "Error: no test specified" && exit 1",
         },
         "dependencies": {
           "extend": "3.0.2"
@@ -158,7 +149,7 @@ test.serial(`If 'package.json' contains a non standard property, it should still
         response: { body: { content: nonStandardJSON } }
     });
 
-    // 3 calls, fetch .json, parse start and parse end.
+    // 3 calls, fetch json, parse start and parse end.
     t.is(t.context.engine.emitAsync.callCount, 3);
-    t.is(t.context.engine.emitAsync.args[2][0], 'parse::end::package-json');    
+    t.is(t.context.engine.emitAsync.args[2][0], 'parse::end::package-json');
 });
