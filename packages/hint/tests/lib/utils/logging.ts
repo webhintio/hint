b3c09d1120150bae17e4925cfc27d832d1118c3f
Tony Ross <antross@gmail.com>
2019-01-07 17:07:43 -0600

    Chore: Deprecate unused `Configuration` methods
    Both `fromFilePath` and `loadBrowsersList` are no longer used by
    the online scanner, CLI, or extensions. Deprecating prior to removal
    since these APIs are public.

--- packages/hint/tests/lib/utils/logging.ts
+++ packages/hint/tests/lib/utils/logging.ts
@@ -7,21 +7,32 @@ test.beforeEach((t) => {
     t.context.console = console;
     console.log = sinon.spy(console, 'log');
     console.error = sinon.spy(console, 'error');
+    console.warn = sinon.spy(console, 'warn');
 });
 
 test.afterEach.always((t) => {
     t.context.console.log.restore();
     t.context.console.error.restore();
+    t.context.console.warn.restore();
 });
 
 test.serial('logging calls console.log', (t) => {
     logging.log('Log');
     t.true(t.context.console.log.calledOnce);
     t.true(t.context.console.error.notCalled);
+    t.true(t.context.console.warn.notCalled);
 });
 
-test.serial('logging calls console.log', (t) => {
+test.serial('logging calls console.error', (t) => {
     logging.error('Error');
     t.true(t.context.console.log.notCalled);
     t.true(t.context.console.error.calledOnce);
+    t.true(t.context.console.warn.notCalled);
+});
+
+test.serial('logging calls console.warn', (t) => {
+    logging.warn('Warn');
+    t.true(t.context.console.log.notCalled);
+    t.true(t.context.console.error.notCalled);
+    t.true(t.context.console.warn.calledOnce);
 });
