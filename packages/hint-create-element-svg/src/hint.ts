16d8ff6822f53789f0e662117705e2b509799f57
utsavized <utsavpandey@gmail.com>
2019-02-20 15:01:54 -0800

    New: get accurate location info for script

--- packages/hint-create-element-svg/src/hint.ts
+++ packages/hint-create-element-svg/src/hint.ts
@@ -3,9 +3,10 @@
  */
 
 import { HintContext } from 'hint/dist/src/lib/hint-context';
-import { IHint } from 'hint/dist/src/lib/types';
+import { IHint, ElementFound, IAsyncHTMLElement, ProblemLocation } from 'hint/dist/src/lib/types';
 import { debug as d } from 'hint/dist/src/lib/utils/debug';
 import { ScriptEvents, ScriptParse } from '@hint/parser-javascript';
+import { findProblemLocation } from 'hint/dist/src/lib/utils/location-helpers';
 
 import { Linter } from 'eslint';
 import * as ESTree from 'estree';
@@ -18,6 +19,11 @@ const debug: debug.IDebugger = d(__filename);
 export default class CreateElementSvgHint implements IHint {
 
     public static readonly meta = meta;
+
+    /** Finds the approximative location in the page's HTML for a match in an element. */
+    public findProblemLocation(element: IAsyncHTMLElement, content?: string): Promise<ProblemLocation> {
+        return findProblemLocation(element, { column: 0, line: 0 }, content);
+    }
 
     public constructor(context: HintContext<ScriptEvents>) {
         const linter = new Linter();
@@ -54,6 +60,8 @@ export default class CreateElementSvgHint implements IHint {
             meta: { messages: { avoidElement: 'Avoid using createElement to create SVG elements; use createElementNS instead' } }
         });
 
+        let scriptElement: IAsyncHTMLElement;
+
         const validateScript = async (scriptData: ScriptParse) => {
 
             debug(`Validating hint create-element-svg`);
@@ -62,10 +70,27 @@ export default class CreateElementSvgHint implements IHint {
             const results = linter.verify(sourceCode, { rules: { 'svg-create': 'error' } });
 
             for (const result of results) {
-                await context.report(scriptData.resource, result.message, { location: { column: result.column, line: result.line }});
+                let position: ProblemLocation | null = scriptElement.getLocation() || null;
+                let line = 0;
+
+                /*
+                 * Eslint returns location data starting at <script>
+                 * This offsets the line number so that it is relative
+                 * to the full source code and not just the <script> tag
+                 */
+                if (scriptElement) {
+                    position = await findProblemLocation(scriptElement, { column: 0, line: 0 }, result.source === null ? '' : result.source);
+                    line = position !== null ? position.line : 0;
+                }
+                await context.report(scriptData.resource, result.message, { location: { column: result.column, line: line + result.line }});
             }
         };
 
+        const enterScript = (elementFound: ElementFound) => {
+            scriptElement = elementFound.element;
+        };
+
+        context.on('element::script', enterScript);
         context.on('parse::end::javascript', validateScript);
     }
 }
