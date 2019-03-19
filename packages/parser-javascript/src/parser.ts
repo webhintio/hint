396fef796123ee3a514290bb319319bbf8525344
Tony Ross <antross@gmail.com>
2019-03-19 14:52:50 -0500

    Breaking: Use acorn directly for JavaScript parsing
    Ref #1499

--- packages/parser-javascript/src/parser.ts
+++ packages/parser-javascript/src/parser.ts
@@ -1,25 +1,16 @@
-import { AST } from 'eslint';
-import SourceCode = require('eslint/lib/util/source-code');
-import * as espree from 'espree';
+import { Options, parse, tokenizer } from 'acorn';
+import * as walk from 'acorn-walk';
+import * as ESTree from 'estree';
 
 import * as logger from 'hint/dist/src/lib/utils/logging';
 import { determineMediaTypeForScript } from 'hint/dist/src/lib/utils/content-type';
-import { HTMLElement, ElementFound, FetchEnd, Parser } from 'hint/dist/src/lib/types';
+import { HTMLElement, ElementFound, FetchEnd, Parser as WebhintParser } from 'hint/dist/src/lib/types';
 import { ScriptEvents } from './types';
 import { Engine } from 'hint';
 
 export * from './types';
 
-// This is the default configuration in eslint for espree.
-const defaultParserOptions = {
-    comment: true,
-    ecmaVersion: 8,
-    loc: true,
-    range: true,
-    tokens: true
-};
-
-export default class JavascriptParser extends Parser<ScriptEvents> {
+export default class JavascriptParser extends WebhintParser<ScriptEvents> {
     public constructor(engine: Engine<ScriptEvents>) {
         super(engine, 'javascript');
 
@@ -27,20 +18,24 @@ export default class JavascriptParser extends Parser<ScriptEvents> {
         engine.on('element::script', this.parseJavascriptTag.bind(this));
     }
 
-    private async emitScript(code: string, resource: string, element: HTMLElement | null) {
+    private async emitScript(sourceCode: string, resource: string, element: HTMLElement | null) {
         try {
             await this.engine.emitAsync(`parse::start::javascript`, { resource });
 
-            const ast: AST.Program = espree.parse(code, defaultParserOptions);
+            const options: Options = { locations: true };
+            const ast = parse(sourceCode, options) as ESTree.Node;
+            const tokens = [...tokenizer(sourceCode, options)];
 
             await this.engine.emitAsync(`parse::end::javascript`, {
                 ast,
                 element,
                 resource,
-                sourceCode: new SourceCode(code, ast)
+                sourceCode,
+                tokens,
+                walk
             });
         } catch (err) {
-            logger.error(`Error parsing JS code: ${code}`);
+            logger.error(`Error parsing JS code: ${sourceCode}`);
         }
     }
 
