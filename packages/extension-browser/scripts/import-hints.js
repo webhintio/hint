webhintio/hint

commit de0f3aedbdbde843d9163b9652337c6724070628

Chore: Panel injects `content-script`
Refactor injection of the content-script from the background script to
the panel, giving the panel control over what code to inject; this also
reduces the number of files included in the bundle and loaded by the
extension.

As part of this change the build is now done in two passes:

1. create the content-script bundle
2. create the extension bundle which `raw-loads` the content-script
   bundle

- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

Close #1801

packages/extension-browser/src/@types/raw-loader.d.ts
@@ -1,0 +1,5 @@
+// Note: raw-loader only works correctly with the bang! syntax
+declare module 'raw-loader!*' {
+    const content: string;
+    export = content;
+}

packages/extension-browser/src/background-script.ts
@@ -25,8 +25,8 @@ const sendEvent = (tabId: number, event: Events) => {
 };
 
 /** Add the script to run webhint to the page. */
-const injectContentScript = (tabId: number, retries = 0) => {
-    browser.tabs.executeScript(tabId, { file: 'content-script/webhint.js', runAt: 'document_start' }, (result) => {
+const injectContentScript = (tabId: number, code: string, retries = 0) => {
+    browser.tabs.executeScript(tabId, { code, runAt: 'document_start' }, (result) => {
         // We get an empty object `{}` back on success, or `undefined` if the script failed to execute.
         if (!result) {
             if (retries <= 2) {
@@ -35,7 +35,7 @@ const injectContentScript = (tabId: number, retries = 0) => {
                  * Variation of https://bugzilla.mozilla.org/show_bug.cgi?id=1397667
                  */
                 console.warn('Failed to inject content script. Retrying...');
-                injectContentScript(tabId, retries + 1);
+                injectContentScript(tabId, code, retries + 1);
             } else {
                 // Give up if retrying still doesn't inject the content script.
                 console.error('Failed to inject content script after retrying.');
@@ -45,7 +45,7 @@ const injectContentScript = (tabId: number, retries = 0) => {
 };
 
 /** Turn on request tracking for the specified tab. */
-const enable = (tabId: number) => {
+const enable = (tabId: number, code: string) => {
     readyTabs.delete(tabId);
     let timeout = 0;
 
@@ -54,7 +54,7 @@ const enable = (tabId: number) => {
         if (details.tabId === tabId && details.frameId === 0) {
             browser.webNavigation.onCommitted.removeListener(onCommitted);
             clearTimeout(timeout);
-            injectContentScript(tabId);
+            injectContentScript(tabId, code);
         }
     };
 
@@ -85,13 +85,13 @@ browser.runtime.onMessage.addListener((message: Events, sender) => {
 
     // Activate content-script when requested by devtools page (saving configuration for when content-script is ready).
     if (message.enable) {
-        configs.set(tabId, message.enable);
-        enable(tabId);
+        configs.set(tabId, message.enable.config);
+        enable(tabId, message.enable.code);
     }
 
     // Forward configuration to content-script when asked (happens before `message.ready`).
     if (message.requestConfig) {
-        const configMessage: Events = { enable: configs.get(tabId) || {} };
+        const configMessage: Events = { config: configs.get(tabId) || {} };
 
         browser.tabs.sendMessage(tabId, configMessage);
     }

packages/extension-browser/src/devtools/panel/panel.ts
@@ -11,6 +11,8 @@ import { syncTheme } from './utils/themes';
 
 import * as styles from './panel.css';
 
+import * as contentScript from 'raw-loader!../../../webhint.js';
+
 document.body.classList.add(styles.root);
 
 /** Display the provided view in the panel. */
@@ -81,7 +83,7 @@ const onCancel = () => {
 /** Handle the user request to start a scan. */
 const onStart = (config: Config) => {
     // Notify the background script to begin scanning with the chosen configuration.
-    sendMessage({ enable: config });
+    sendMessage({ enable: { code: contentScript, config } });
 
     // Listen for scan results and network requests (to create `fetch::end::*` events).
     addMessageListener(onMessage);

packages/extension-browser/src/shared/types.ts
@@ -6,6 +6,11 @@ export type Config = {
     ignoredUrls?: string;
 };
 
+export type InjectDetails = {
+    code: string;
+    config: Config;
+}
+
 export type HintResults = {
     helpURL: string;
     id: string;
@@ -24,7 +29,8 @@ export type Results = {
 };
 
 export type Events = {
-    enable?: Config;
+    config?: Config;
+    enable?: InjectDetails;
     fetchEnd?: FetchEnd;
     fetchStart?: FetchStart;
     done?: boolean;

packages/extension-browser/webpack.config.js
@@ -1,12 +1,6 @@
 const path = require('path');
 
-module.exports = {
-    entry: {
-        'background-script': './dist/src/background-script.js',
-        'content-script/webhint': './dist/src/content-script/webhint.js',
-        'devtools/devtools': './dist/src/devtools/devtools.js',
-        'devtools/panel/panel': './dist/src/devtools/panel/panel.js'
-    },
+const baseConfig = {
     mode: 'none',
     module: {
         rules: [
@@ -57,4 +51,31 @@ module.exports = {
         path: path.resolve(__dirname, 'dist/bundle')
     },
     resolve: { alias: { url$: path.resolve(__dirname, 'dist/src/shims/url.js') } }
+};
+
+const contentScriptConfig = {
+    ...baseConfig,
+    ...{
+        entry: { webhint: './dist/src/content-script/webhint.js' },
+        output: { ...baseConfig.output, path: path.resolve(__dirname, 'dist') }
+    }
+};
+
+const extensionConfig = {
+    ...baseConfig,
+    ...{
+        entry: {
+            'background-script': './dist/src/background-script.js',
+            'devtools/devtools': './dist/src/devtools/devtools.js',
+            'devtools/panel/panel': './dist/src/devtools/panel/panel.js'
+        }
+    }
+};
+
+module.exports = (env) => {
+    if (env && env['content-script']) {
+        return contentScriptConfig;
+    }
+
+    return extensionConfig;
 };

commit b5fe7e15543bf65bb4f4ab8426bf4f0bacb122f6

Chore: Reduce size of packed MDN data
Ref #1499

Also fix ability to debug the content script which was broken by #1801.

Also minify browser extension when building for production. Includes
fixing handling of non-ASCII characters in minified content script.

packages/extension-browser/scripts/import-hints.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-const package = require('../package.json');
+const pkg = require('../package.json');
 
-const hintModules = Object.keys(package.devDependencies).filter((name) => {
+const hintModules = Object.keys(pkg.devDependencies).filter((name) => {
     return name.startsWith('@hint/hint-');
 });
 

packages/extension-browser/scripts/pack-mdn-data.js
@@ -3,11 +3,242 @@ const mdn = require('mdn-browser-compat-data');
 const path = require('path');
 const filename = path.resolve(`${__dirname}/../dist/mdn-browser-compat-data.packed.json`);
 
-fs.writeFile(filename, JSON.stringify({
+/**
+ * Determine if a given support statement qualifies as "always supported" by
+ * the specified browser.
+ *
+ * @param {string} browserName
+ * @param {bcd.SimpleSupportStatement} supportStatement
+ * @returns {boolean}
+ */
+const isUniversalSupportStatement = (browserName, supportStatement) => {
+    const version = supportStatement.version_added;
+
+    if (supportStatement.version_removed) {
+        return false;
+    }
+
+    if (typeof version === 'boolean') {
+        return version;
+    }
+
+    if (version === null) {
+        return true; // Count unknown as universal support.
+    }
+
+    if (version === '1') {
+        return true;
+    }
+
+    // Treat features in select minimum browser versions as universally supported.
+    switch (browserName) {
+
+        case 'chrome':
+        case 'chrome_android':
+        case 'opera':
+        case 'opera_android':
+            return parseInt(version) <= 49;
+
+        case 'edge':
+        case 'edge_mobile':
+            return version === '12';
+
+        case 'firefox':
+        case 'firefox_android':
+            return parseInt(version) <= 49;
+
+        case 'ie':
+            return parseInt(version) <= 9;
+
+        case 'safari':
+        case 'safari_ios':
+            return parseInt(version) <= 9;
+
+        case 'webview_android':
+            return parseInt(version) <= 4;
+
+        default:
+            return false;
+    }
+};
+
+/**
+ * Extract an array of all support statements for the given browser.
+ * Used to disambiguate between different data representations.
+ *
+ * @param {string} browserName
+ * @param {bcd.SupportBlock} support
+ * @returns {bcd.SimpleSupportStatement[]}
+ */
+const getSupportStatements = (browserName, support) => {
+    const browserData = support[browserName] || [];
+
+    return Array.isArray(browserData) ? browserData : [browserData];
+};
+
+/**
+ * Determine if a feature is supported by all min-browsers.
+ *
+ * @param {bcd.CompatStatement} compat
+ * @returns {boolean}
+ */
+const isFeatureUniversallySupported = (compat) => {
+    for (const browserName of Object.keys(compat.support)) {
+        for (const supportStatement of getSupportStatements(browserName, compat.support)) {
+            if (!isUniversalSupportStatement(browserName, supportStatement)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
+
+/**
+ * Determine if a feature is *not* supported anywhere.
+ *
+ * @param {bcd.CompatStatement} compat
+ */
+const isFeatureUniversallyUnsupported = (compat) => {
+    for (const browserName of Object.keys(compat.support)) {
+        const supportStatements = getSupportStatements(browserName, compat.support);
+
+        if (supportStatements.length > 1 || supportStatements[0].version_added) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+/**
+ * Remove any `bcd.SimpleSupportStatement` that requires `flags`.
+ * Flagged entries are ignored since they must be enabled by end users.
+ *
+ * @param {string} browserName
+ * @param {bcd.SupportBlock} support
+ */
+const removeFlaggedSupport = (browserName, support) => {
+    const supportStatements = getSupportStatements(browserName, support);
+    const unflaggedSupportStatements = supportStatements.filter((supportStatement) => {
+        return !supportStatement.flags;
+    });
+
+    if (unflaggedSupportStatements.length) {
+        support[browserName] = unflaggedSupportStatements;
+    } else {
+        delete support[browserName];
+    }
+};
+
+/**
+ * Remove all notes as they are not useful for analysis.
+ *
+ * @param {string} browserName
+ * @param {bcd.SupportBlock} support
+ */
+const removeSupportNotes = (browserName, support) => {
+    for (const supportStatement of getSupportStatements(browserName, support)) {
+        delete supportStatement.notes;
+    }
+};
+
+/**
+ * Remove all support statements which indicate universal support.
+ *
+ * @param {string} browserName
+ * @param {bcd.SupportBlock} support
+ */
+const removeUniversalSupport = (browserName, support) => {
+    for (const supportStatement of getSupportStatements(browserName, support)) {
+        if (isUniversalSupportStatement(browserName, supportStatement)) {
+            delete support[browserName];
+
+            return;
+        }
+    }
+};
+
+/**
+ * Remove all data about a feature which is unnecessary for analysis.
+ *
+ * @param {bcd.Identifier} data
+ */
+const removeFeatureData = (data) => {
+    if (!data.__compat) {
+        return;
+    }
+
+    const compat = data.__compat;
+    const support = data.__compat.support;
+
+    // Remove unnecessary data cross-browser.
+    if (isFeatureUniversallySupported(compat) || isFeatureUniversallyUnsupported(compat)) {
+        delete data.__compat;
+
+        return;
+    }
+
+    // Description is not needed for analysis.
+    delete compat.description;
+
+    // Remove unnecessary data per-browser.
+    for (const browserName of Object.keys(support)) {
+        removeFlaggedSupport(browserName, support);
+        removeUniversalSupport(browserName, support);
+        removeSupportNotes(browserName, support);
+    }
+
+    // Remove `__compat` if all browsers have been removed.
+    if (!Object.keys(support).length) {
+        delete data.__compat;
+    }
+};
+
+/**
+ * Remove all features and data which are unnecessary for analysis.
+ * E.g. feature is universally supported or universally unsupported.
+ *
+ * @param {bcd.Identifier} data
+ */
+const removeFeatures = (data) => {
+    for (const key of Object.keys(data)) {
+        if (key === '__compat') {
+            removeFeatureData(data);
+        } else {
+            removeFeatures(data[key]);
+
+            if (!Object.keys(data[key]).length) {
+                delete data[key];
+            }
+        }
+    }
+};
+
+/**
+ * Strip browser details down to name only.
+ *
+ * @param {bcd.Browsers} browsers
+ */
+const removeBrowserDetails = (browsers) => {
+    for (const browserName of Object.keys(browsers)) {
+        browsers[browserName] = /** @type {any} */({});
+    }
+};
+
+// Remove unnecessary data
+
+const data = {
     browsers: mdn.browsers,
     css: mdn.css,
     html: mdn.html
-}), (err) => {
+};
+
+removeBrowserDetails(data.browsers);
+removeFeatures(data.css);
+removeFeatures(data.html);
+
+fs.writeFile(filename, JSON.stringify(data), (err) => {
     if (err) {
         throw err;
     } else {

packages/extension-browser/src/@types/raw-loader.d.ts
@@ -1,5 +1,0 @@
-// Note: raw-loader only works correctly with the bang! syntax
-declare module 'raw-loader!*' {
-    const content: string;
-    export = content;
-}

packages/extension-browser/src/background-script.ts
@@ -25,8 +25,8 @@ const sendEvent = (tabId: number, event: Events) => {
 };
 
 /** Add the script to run webhint to the page. */
-const injectContentScript = (tabId: number, code: string, retries = 0) => {
-    browser.tabs.executeScript(tabId, { code, runAt: 'document_start' }, (result) => {
+const injectContentScript = (tabId: number, retries = 0) => {
+    browser.tabs.executeScript(tabId, { file: 'content-script/webhint.js', runAt: 'document_start' }, (result) => {
         // We get an empty object `{}` back on success, or `undefined` if the script failed to execute.
         if (!result) {
             if (retries <= 2) {
@@ -35,7 +35,7 @@ const injectContentScript = (tabId: number, code: string, retries = 0) => {
                  * Variation of https://bugzilla.mozilla.org/show_bug.cgi?id=1397667
                  */
                 console.warn('Failed to inject content script. Retrying...');
-                injectContentScript(tabId, code, retries + 1);
+                injectContentScript(tabId, retries + 1);
             } else {
                 // Give up if retrying still doesn't inject the content script.
                 console.error('Failed to inject content script after retrying.');
@@ -45,7 +45,7 @@ const injectContentScript = (tabId: number, code: string, retries = 0) => {
 };
 
 /** Turn on request tracking for the specified tab. */
-const enable = (tabId: number, code: string) => {
+const enable = (tabId: number) => {
     readyTabs.delete(tabId);
     let timeout = 0;
 
@@ -54,7 +54,7 @@ const enable = (tabId: number, code: string) => {
         if (details.tabId === tabId && details.frameId === 0) {
             browser.webNavigation.onCommitted.removeListener(onCommitted);
             clearTimeout(timeout);
-            injectContentScript(tabId, code);
+            injectContentScript(tabId);
         }
     };
 
@@ -86,7 +86,7 @@ browser.runtime.onMessage.addListener((message: Events, sender) => {
     // Activate content-script when requested by devtools page (saving configuration for when content-script is ready).
     if (message.enable) {
         configs.set(tabId, message.enable.config);
-        enable(tabId, message.enable.code);
+        enable(tabId);
     }
 
     // Forward configuration to content-script when asked (happens before `message.ready`).

packages/extension-browser/src/devtools/panel/panel.ts
@@ -12,8 +12,6 @@ import { syncTheme } from './utils/themes';
 
 import * as styles from './panel.css';
 
-import * as contentScript from 'raw-loader!../../../webhint.js';
-
 document.body.classList.add(styles.root);
 
 /** Display the provided view in the panel. */
@@ -107,7 +105,7 @@ const onCancel = () => {
 /** Handle the user request to start a scan. */
 const onStart = (config: Config) => {
     // Notify the background script to begin scanning with the chosen configuration.
-    sendMessage({ enable: { code: contentScript, config } });
+    sendMessage({ enable: { config } });
 
     // Listen for scan results and network requests (to create `fetch::end::*` events).
     addMessageListener(onMessage);

packages/extension-browser/src/shared/types.ts
@@ -8,7 +8,6 @@ export type Config = {
 };
 
 export type InjectDetails = {
-    code: string;
     config: Config;
 }
 

packages/extension-browser/webpack.config.js
@@ -1,96 +1,88 @@
 const path = require('path');
 const webpack = require('webpack');
-
-const baseConfig = {
-    mode: 'none',
-    module: {
-        rules: [
-            // Bundle `axe-core` as a raw string so it can be injected at runtime.
-            {
-                test: /axe-core/,
-                use: 'raw-loader'
-            },
-            // Bundle `js-library-detector as a raw string so it can be injected at runtime.
-            {
-                test: /js-library-detector/,
-                use: 'raw-loader'
-            },
-            // Automatically bundle and inject referenced CSS files.
-            {
-                exclude: /highlight\.js[/\\]styles/,
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            /*
-                             * Generate scoped class names using the same format as `create-react-app` (unique, but readable).
-                             * https://facebook.github.io/create-react-app/docs/adding-a-css-modules-stylesheet
-                             */
-                            localIdentName: '[name]_[local]_[hash:base64:5]',
-                            modules: true
-                        }
-                    }
-                ]
-            },
-            // Bundle highlight.js styles with `Useable` so they can be turned on/off.
-            {
-                test: /highlight\.js[/\\]styles/,
-                use: [
-                    'style-loader/useable',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.svg$/,
-                use: {
-                    loader: 'svg-url-loader',
-                    options: { noquotes: true }
-                }
-            }
-        ]
-    },
-    node: { fs: 'empty' },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist/bundle')
-    },
-    plugins: [
-        new webpack.DefinePlugin({ 'process.env.webpack': JSON.stringify(true) })
-    ],
-    resolve: {
-        alias: {
-            'hint/dist/src/lib/utils/network/request-async$': path.resolve(__dirname, 'dist/src/shims/request-async.js'),
-            'mdn-browser-compat-data$': path.resolve(__dirname, 'dist/mdn-browser-compat-data.packed.json'),
-            url$: path.resolve(__dirname, 'dist/src/shims/url.js')
-        }
-    }
-};
-
-const contentScriptConfig = {
-    ...baseConfig,
-    ...{
-        entry: { webhint: './dist/src/content-script/webhint.js' },
-        output: { ...baseConfig.output, path: path.resolve(__dirname, 'dist') }
-    }
-};
-
-const extensionConfig = {
-    ...baseConfig,
-    ...{
-        entry: {
-            'background-script': './dist/src/background-script.js',
-            'devtools/devtools': './dist/src/devtools/devtools.js',
-            'devtools/panel/panel': './dist/src/devtools/panel/panel.js'
-        }
-    }
-};
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = (env) => {
-    if (env && env['content-script']) {
-        return contentScriptConfig;
-    }
-
-    return extensionConfig;
+    return {
+        entry: {
+            'background-script': './dist/src/background-script.js',
+            'content-script/webhint': './dist/src/content-script/webhint.js',
+            'devtools/devtools': './dist/src/devtools/devtools.js',
+            'devtools/panel/panel': './dist/src/devtools/panel/panel.js'
+        },
+        mode: env && env.release ? 'production' : 'none',
+        module: {
+            rules: [
+                // Bundle `axe-core` as a raw string so it can be injected at runtime.
+                {
+                    test: /axe-core/,
+                    use: 'raw-loader'
+                },
+                // Bundle `js-library-detector as a raw string so it can be injected at runtime.
+                {
+                    test: /js-library-detector/,
+                    use: 'raw-loader'
+                },
+                // Automatically bundle and inject referenced CSS files.
+                {
+                    exclude: /highlight\.js[/\\]styles/,
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                /*
+                                 * Generate scoped class names using the same format as `create-react-app` (unique, but readable).
+                                 * https://facebook.github.io/create-react-app/docs/adding-a-css-modules-stylesheet
+                                 */
+                                localIdentName: '[name]_[local]_[hash:base64:5]',
+                                modules: true
+                            }
+                        }
+                    ]
+                },
+                // Bundle highlight.js styles with `Useable` so they can be turned on/off.
+                {
+                    test: /highlight\.js[/\\]styles/,
+                    use: [
+                        'style-loader/useable',
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.svg$/,
+                    use: {
+                        loader: 'svg-url-loader',
+                        options: { noquotes: true }
+                    }
+                }
+            ]
+        },
+        node: { fs: 'empty' },
+        optimization: {
+            minimizer: [
+                /*
+                 * Fix handling of non-ASCII characters in minified content script
+                 * when running in Chrome by forcing them to be escaped.
+                 */
+                // eslint-disable-next-line camelcase
+                new TerserPlugin({ terserOptions: { output: { ascii_only: true } } })
+            ]
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist/bundle')
+        },
+        plugins: [
+            new webpack.DefinePlugin({ 'process.env.webpack': JSON.stringify(true) })
+        ],
+        resolve: {
+            alias: {
+                'hint/dist/src/lib/utils/network/request-async$': path.resolve(__dirname, 'dist/src/shims/request-async.js'),
+                'mdn-browser-compat-data$': path.resolve(__dirname, 'dist/mdn-browser-compat-data.packed.json'),
+                url$: path.resolve(__dirname, 'dist/src/shims/url.js')
+            }
+        }
+    };
 };

packages/extension-browser/tests/background-script.ts
@@ -9,8 +9,7 @@ import { Config, Events, Results } from '../src/shared/types';
 import { awaitListener, stubEvent, stubGlobals, Globals } from './helpers/globals';
 
 const backgroundScriptPath = '../src/background-script';
-
-const contentScript = 'DUMMY CONTENT SCRIPT';
+const contentScriptPath = 'content-script/webhint.js';
 
 /**
  * Require the background script using the provided `browser` APIs.
@@ -42,7 +41,7 @@ const prepareContentScriptInjection = (sandbox: SinonSandbox, browser: typeof ch
         const onMessage = await onMessagePromise;
 
         // Simulate receiving `Events.enable` from the devtools panel.
-        onMessage({ enable: { code: contentScript, config: {} } } as Events, { tab: { id: tabId } } as any, () => {});
+        onMessage({ enable: { config: {} } } as Events, { tab: { id: tabId } } as any, () => {});
 
         const onCommitted = await onCommittedPromise;
 
@@ -105,7 +104,7 @@ test('It injects the content script when enabled.', async (t) => {
 
     t.true(executeScriptSpy.calledOnce);
     t.is(executeScriptSpy.firstCall.args[0], tabId);
-    t.is(executeScriptSpy.firstCall.args[1].code, contentScript);
+    t.is(executeScriptSpy.firstCall.args[1].file, contentScriptPath);
     t.is(executeScriptSpy.firstCall.args[1].runAt, 'document_start');
 
     sandbox.restore();
@@ -139,7 +138,7 @@ test('It retries injecting the content script if it fails.', async (t) => {
 
     t.true(executeScriptSpy.calledOnce);
     t.is(executeScriptSpy.firstCall.args[0], tabId);
-    t.is(executeScriptSpy.firstCall.args[1].code, contentScript);
+    t.is(executeScriptSpy.firstCall.args[1].file, contentScriptPath);
     t.is(executeScriptSpy.firstCall.args[1].runAt, 'document_start');
 
     sandbox.restore();
@@ -164,7 +163,7 @@ test('It passes provided configuration to the content script.', async (t) => {
     const onMessage = await onMessagePromise;
 
     // Simulate receiving `Events.enable` from the devtools panel.
-    onMessage({ enable: { code: contentScript, config }, tabId } as Events, {} as any, () => {});
+    onMessage({ enable: { config }, tabId } as Events, {} as any, () => {});
 
     // Simulate receiving `Events.requestConfig` from the content script.
     onMessage({ requestConfig: true}, { tab: { id: tabId } } as any, () => {});

packages/extension-browser/tests/end-to-end.ts
@@ -1,9 +1,6 @@
 import * as isCI from 'is-ci';
 import { launch, Browser, Page } from 'puppeteer';
 import test from 'ava';
-import { resolve } from 'path';
-
-import readFile from 'hint/dist/src/lib/utils/fs/read-file';
 
 import { Server } from '@hint/utils-create-server';
 
@@ -12,8 +9,6 @@ import { Events, Results } from '../src/shared/types';
 import { readFixture } from './helpers/fixtures';
 
 const pathToExtension = `${__dirname}/../bundle`;
-
-const contentScript = readFile(resolve(`${__dirname}/../webhint.js`));
 
 /**
  * Find the Puppeteer `Page` associated with the background script
@@ -82,7 +77,7 @@ test('It runs end-to-end in a page', async (t) => {
         });
     });
 
-    await page.addScriptTag({ path: `${__dirname}/../webhint.js` });
+    await page.addScriptTag({ path: `${__dirname}/../bundle/content-script/webhint.js` });
 
     const results = await resultsPromise;
 
@@ -123,7 +118,7 @@ if (!isCI) {
             setTimeout(resolve, 500);
         });
 
-        const results: Results = await backgroundPage.evaluate((code) => {
+        const results: Results = await backgroundPage.evaluate(() => {
             return new Promise<Results>((resolve) => {
                 chrome.runtime.onMessage.addListener((message: Events) => {
                     if (message.results) {
@@ -131,17 +126,12 @@ if (!isCI) {
                     }
                 });
 
-                const event: Events = {
-                    enable: {
-                        code,
-                        config: {}
-                    }
-                };
+                const event: Events = { enable: { config: {} } };
 
                 // Simulate sending message from devtools panel to background script to start analyzing.
                 chrome.tabs.executeScript({ code: `chrome.runtime.sendMessage(${JSON.stringify(event)})` });
             });
-        }, contentScript);
+        });
 
         t.not(results.categories.length, 0);
         t.true(results.categories.some((category) => {