ef47711f5356401f3dfbf83c9b7b4881abeb4ee8
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2019-04-12 16:30:15 -0700

    Chore: Add comments

--- packages/parser-javascript/src/parser.ts
+++ packages/parser-javascript/src/parser.ts
@@ -18,6 +18,17 @@ type Key = {
     state?: any;
 };
 
+/**
+ * A WalkArray is a pair of Key and a Map with
+ * all the callbacks (for methods full and fullAncestor)
+ * or all the NodeVisitor (for methods simple and ancestor) for that Key.
+ *
+ * The key is the same if the root node, the base and the state
+ * you call walk.(simple|ancestor|full|fullAncestor) is the same.
+ *
+ * In case of method used is `full` or `fullAncestor`, the map will
+ * have an only key `callbacks`.
+ */
 type WalkArray = Array<[Key, Map<string, Array<Function>>]>;
 
 export default class JavascriptParser extends WebhintParser<ScriptEvents> {
@@ -44,13 +55,63 @@ export default class JavascriptParser extends WebhintParser<ScriptEvents> {
             const ast = parse(sourceCode, options) as ESTree.Node;
             const tokens = [...tokenizer(sourceCode, options)];
 
+            // Store a WalkArray for each method supported.
             const walkArrays: { [t: string]: WalkArray } = {};
 
+            /**
+             * Create a method that will create a WalkArray for a walk method (simple, full, etc.).
+             */
             const getWalkAccumulator = (methodName: string) => {
                 if (!walkArrays[methodName]) {
                     walkArrays[methodName] = [];
                 }
 
+                /**
+                 * Every time a hint calls to walk.(simple|ancestor|full|fullAncestor), it is going to
+                 * execute this method, storing in a WalkArray object all the NodeVistors or Callbacks
+                 * the hints are defining for the walk method.
+                 *
+                 * This will allow later generate our custom NodeVisitor(s) or callback to call the
+                 * real `acorn-walk` method, so we just need to walk once for each Key (node + base + state)
+                 * and method.
+                 *
+                 * E.g:
+                 * For the script1.js
+                 *
+                 * hint 1: call to walk.simple(ast, {
+                 *     CallExpression(node) { // this is a NodeVisitor
+                 *         // any code here
+                 *     }
+                 * });
+                 *
+                 * hint 2: call to walk.simple(ast, {
+                 *     CallExpression(node) { // This is a NodeVisitor
+                 *         // any other code here
+                 *     }
+                 * });
+                 *
+                 * hint 3: call to walk.simple(ast, {
+                 *     Literal(node) { // This is a NodeVisitor
+                 *         // any code for Literal.
+                 *     }
+                 * });
+                 *
+                 * hint 3: call to walk.full(ast, (node) => {
+                 *   // Callback code here.
+                 * });
+                 *
+                 * These hints will create two WalkArray, one for the method `simple` and another one
+                 * for the method `full`.
+                 *
+                 * The WalkArray object for `simple` will have a map with 2 entries, the key of the first entry
+                 * will be `CallExpression` and the content for that entry will be an array with 2 NodeVisitors,
+                 * one from `hint 1`, and another from the `hint 2`. The key for the second entry will be `Literal`
+                 * and the content for that entry will be an array with 1 NodeVisitor the one from `hint 3`
+                 *
+                 * The WalkArray object for `full` will hava a map with 1 entry, the key of that entry will
+                 * be `callbacks`, and the content for that entry will be an array with 1 function. That function
+                 * is the function defined in `hint 4`
+                 */
                 return (node: ESTree.Node, visitorsOrCallback: NodeVisitor | Function, base?: NodeVisitor, state?: any) => {
                     let currentVisitors = this.getCurrentVisitorsOrCallback(walkArrays[methodName], node, base, state);
 
@@ -105,6 +166,27 @@ export default class JavascriptParser extends WebhintParser<ScriptEvents> {
                 walk
             });
 
+            /**
+             * After all the hints has register their NodeVisitor or callback,
+             * it is time to generate a single NodeVisitor for each different
+             * NodeVisitor registered by the hints and execute the real walk.
+             *
+             * Continuing with the previous example, this code will execute:
+             *
+             * acornWalk.simple(ast, {
+             *     CallExpresion(node) {
+             *         // code from CallExpression in hint 1
+             *         // code from CallExpression in hint 2
+             *     },
+             *     Literal(node) {
+             *         // code from Literal in hint 3
+             *     }
+             * });
+             *
+             * acornWalk.ful(ast, (node) => {
+             *     // code from callback in hint 4
+             * });
+             */
             Object.entries(walkArrays).forEach(([methodName, walkArray]) => {
                 walkArray.forEach(([{ node, state, base }, visitors]) => {
                     let allVisitors: NodeVisitor | Function = {};
