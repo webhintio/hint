a0b11b7a8c857de53123ea11c05a782b4ded78b8
Tony Ross <antross@gmail.com>
2019-04-18 17:29:33 -0500

    Chore: Inline analytics script
    Also switch to using the "basic" v2 version of Application Insights.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Fix #2230
    Close #2266

--- packages/extension-browser/src/devtools/utils/analytics.ts
+++ packages/extension-browser/src/devtools/utils/analytics.ts
@@ -1,33 +1,53 @@
-import { AppInsights } from 'applicationinsights-js';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web-basic';
+
+const instrumentationKey = '8ef2b55b-2ce9-4c33-a09a-2c3ef605c97d';
+
+let appInsights: ApplicationInsights;
+
+const trackEvent = (name: string, properties?: { [key: string]: string }, measurements?: { [key: string]: number }) => {
+    if (!appInsights) {
+        return;
+    }
+
+    appInsights.track({
+        baseData: {
+            measurements,
+            name,
+            properties
+        },
+        baseType: 'EventData',
+        name: `Microsoft.ApplicationInsights.${instrumentationKey}.Event`
+    });
+};
 
 import { ErrorData } from '../../shared/types';
 
 /** Called to initialize the underlying analytics library. */
 export const setup = () => {
-    AppInsights.downloadAndSetup!({ instrumentationKey: '8ef2b55b-2ce9-4c33-a09a-2c3ef605c97d' });
+    appInsights = new ApplicationInsights({ instrumentationKey });
 };
 
 /** Called when analysis was canceled by the user. */
 export const trackCancel = (duration: number) => {
-    AppInsights.trackEvent('f12-cancel', undefined, { 'f12-cancel-duration': duration });
+    trackEvent('f12-cancel', undefined, { 'f12-cancel-duration': duration });
 };
 
 /** Called when analysis fails due to an error. */
 export const trackError = (error: ErrorData) => {
-    AppInsights.trackEvent('f12-error', error);
+    trackEvent('f12-error', error);
 };
 
 /** Called when analysis finished. */
 export const trackFinish = (duration: number) => {
-    AppInsights.trackEvent('f12-finish', undefined, { 'f12-finish-duration': duration });
+    trackEvent('f12-finish', undefined, { 'f12-finish-duration': duration });
 };
 
 /** Called when the "Hints" tab was opened by the user. */
 export const trackShow = () => {
-    AppInsights.trackEvent('f12-show');
+    trackEvent('f12-show');
 };
 
 /** Called when analysis was started by the user. */
 export const trackStart = () => {
-    AppInsights.trackEvent('f12-start');
+    trackEvent('f12-start');
 };
