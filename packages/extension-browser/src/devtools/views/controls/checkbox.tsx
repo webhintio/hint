87fbf13bd3f155c8ad751b6ef7f86b5408564f76
Tony Ross <antross@gmail.com>
2019-05-09 17:37:24 -0500

    Chore: Rename a bunch of CSS variables for consistency

--- packages/extension-browser/src/devtools/views/controls/checkbox.tsx
+++ packages/extension-browser/src/devtools/views/controls/checkbox.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 
+import { useCurrentDesign } from '../../utils/themes';
 import { ElementProps, Omit } from '../../utils/types';
 
-import * as styles from './checkbox.css';
+import * as fluent from './checkbox.fluent.css';
+import * as photon from './checkbox.photon.css';
 
 type Props = Omit<'type', ElementProps<'input'>>;
 
@@ -10,6 +12,8 @@ type Props = Omit<'type', ElementProps<'input'>>;
  * Checkbox input with common styles.
  */
 const Checkbox = ({ className, ...props }: Props) => {
+    const styles = useCurrentDesign({ fluent, photon });
+
     return (
         <input type="checkbox" className={`${styles.root} ${className || ''}`} {...props} />
     );
