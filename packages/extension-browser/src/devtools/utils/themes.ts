ad2873b3b453f52a3cf6c6eebb14214bc5fcdd0d
Tony Ross <antross@gmail.com>
2019-05-09 17:35:35 -0500

    New: Infrastructure for multiple design systems
    Fix #1670
    
    Support for changing the target design system via a build flag. Provides
    a React Hook for components to use to choose between styles for the
    specified design system.
    
    Usage:
    ```ts
    import * as fluent from 'component.fluent.css';
    import * as photon from 'component.photon.css';
    
    const styles = useCurrentDesign({ fluent, photon });
    ```

--- packages/extension-browser/src/devtools/utils/themes.ts
+++ packages/extension-browser/src/devtools/utils/themes.ts
@@ -2,6 +2,30 @@ import { useEffect, useState } from 'react';
 
 import { browser } from '../../shared/globals';
 
+/** Supported design systems. */
+type DesignSystems<T> = {
+    fluent: T;
+    photon?: T;
+};
+
+/** Current design system. */
+declare const DESIGN_SYSTEM: keyof DesignSystems<any>;
+
+/**
+ * Select styles to use based on the current design system.
+ * Falls back to the default design system if no matching styles were found.
+ *
+ * ```ts
+ * import * as fluent from 'component.fluent.css';
+ * import * as photon from 'component.photon.css';
+ *
+ * const styles = useCurrentDesign({ fluent, photon });
+ * ```
+ */
+export const useCurrentDesign = <T>(designs: DesignSystems<T>): T => {
+    return designs[DESIGN_SYSTEM] || designs.fluent;
+};
+
 /** Align active styles with the current devtools theme. */
 export const useCurrentTheme = () => {
     const [theme, setTheme] = useState(browser.devtools.panels.themeName);
@@ -20,5 +44,5 @@ export const useCurrentTheme = () => {
         };
     }, []); // Only register on mount and cleanup on unmount.
 
-    return theme;
+    return theme.startsWith('dark') ? 'dark' : 'light';
 };
