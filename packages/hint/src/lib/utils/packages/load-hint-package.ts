00c5729dce587b6664b2875fe438e5a748301e98
Tony Ross <antross@gmail.com>
2019-05-22 17:45:59 -0500

    Chore: Fix reference to package.json when bundled

--- packages/hint/src/lib/utils/packages/load-hint-package.ts
+++ packages/hint/src/lib/utils/packages/load-hint-package.ts
@@ -7,7 +7,7 @@ export const loadHintPackage = () => {
     // webpack will embed the package.json
     /* istanbul ignore if */
     if (process.env.webpack) { // eslint-disable-line no-process-env
-        return require('../../../../hint/package.json');
+        return require('../../../../../../hint/package.json');
     }
 
     const pkgPath = findPackageRoot(__dirname);
