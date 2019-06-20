9fa84ec1ea5993ebb1e65cfa6aa84bfa1d824f1c
Tony Ross <antross@gmail.com>
2019-06-20 11:49:52 -0500

    Chore: Update hint-create-element-svg imports to allow bundling

--- packages/hint-create-element-svg/src/meta.ts
+++ packages/hint-create-element-svg/src/meta.ts
@@ -1,4 +1,6 @@
-import { Category, HintMetadata, HintScope } from 'hint';
+import { Category } from 'hint/dist/src/lib/enums/category';
+import { HintScope } from 'hint/dist/src/lib/enums/hint-scope';
+import { HintMetadata } from 'hint/dist/src/lib/types';
 
 const meta: HintMetadata = {
     docs: {
