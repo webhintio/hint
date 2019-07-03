65f1c582aff27b3e84b182472d4b9730a2385380
Tony Ross <antross@gmail.com>
2019-07-03 15:41:00 -0500

    Chore: Sort packages by version delta during release validation

--- release/tasks/validate-changes.ts
+++ release/tasks/validate-changes.ts
@@ -1,7 +1,20 @@
+import * as semver from 'semver';
+
 import { Context } from '../@types/custom';
 import { debug } from '../lib/utils';
 
 const inquirer = require('listr-inquirer'); // `require` used because `listr-inquirer` exports a function
+
+const delta = (oldVersion: string, newVersion: string): string => {
+    const svOld = semver.parse(oldVersion)!;
+    const svNew = semver.parse(newVersion)!;
+
+    const dMajor = svNew.major - svOld.major;
+    const dMinor = Math.max(0, svNew.minor - svOld.minor);
+    const dPatch = Math.max(0, svNew.patch - svOld.patch);
+
+    return `${dMajor}.${dMinor}.${dPatch}`;
+};
 
 /**
  * Prompts the user if the changes are correct
@@ -15,9 +28,16 @@ export const validateChanges = (ctx: Context) => {
         maxLength = Math.max(name.length, maxLength);
     }
 
-    for (const [, pkg] of ctx.packages) {
+    const packages = [...ctx.packages.values()].sort((p1, p2) => {
+        const d1 = delta(p1.oldVersion, p1.content.version);
+        const d2 = delta(p2.oldVersion, p2.content.version);
+
+        return semver.compare(d2, d1) || p1.name.localeCompare(p2.name);
+    });
+
+    for (const pkg of packages) {
         if (!pkg.ignore && (pkg.updated || !pkg.publishedVersion)) {
-            debug(`${pkg.name.padEnd(maxLength)} ${pkg.oldVersion.padEnd(8)} ${pkg.content.version}`);
+            debug(`${pkg.name.padEnd(maxLength)} ${pkg.oldVersion.padEnd(8)} ${pkg.content.version.padEnd(8)} (${semver.diff(pkg.oldVersion, pkg.content.version)})`);
         }
     }
 
