e147dbbe85b448fd9324cc016bf8f99c357e1b1b
Tony Ross <antross@gmail.com>
2019-07-11 13:48:59 -0500

    New: Add fixing state to hint status metrics
    Tracks progress partially fixing issues reported by a hint.
    
    - - - - - - - - - -
    
    Fix #2678

--- packages/extension-browser/src/devtools/utils/hints.ts
+++ packages/extension-browser/src/devtools/utils/hints.ts
@@ -7,37 +7,52 @@ import * as storage from './storage';
 export const enum HintStatus {
     passed = 'passed',
     failed = 'failed',
-    fixed = 'fixed'
+    fixed = 'fixed',
+    fixing = 'fixing'
+}
+
+type ScanStatus = {
+    config?: Config;
+    hints: { [id: string]: number };
+    url?: string;
 }
 
 const statusKey = `webhint-hint-status`;
 
 /**
- * Extract pass/fail/fixed status for each hint from scan results.
+ * Extract pass/fail/fixing/fixed status for each hint from scan results.
  * A hint is considered fixed when passing after failing the previous scan.
+ * Fixing is when the number of failures decreases between scans.
  *
- * To minimize false-positives for the 'fixed' status, the previous scan must
- * occur on the same URL with the same configuration.
+ * To minimize false-positives for the 'fixing' and 'fixed' statuses, the
+ * previous scan must occur on the same URL with the same configuration.
  */
 export const determineHintStatus = (config: Config, results: Results, s = storage): { [key: string]: HintStatus } => {
     const hintStatus: { [key: string]: HintStatus } = {};
-    const status = { config, url: results.url };
+    const status: ScanStatus = { config, hints: {}, url: results.url };
 
-    let prevStatus: { [key: string]: string | undefined } = s.getItem(statusKey) || {};
+    let saved: ScanStatus | undefined = s.getItem(statusKey);
 
     // If the configuration has changed, ignore the previous scan.
-    if (status.url !== prevStatus.url || !isEqual(prevStatus.config, status.config)) {
-        prevStatus = {};
+    if (!saved || !saved.hints || status.url !== saved.url || !isEqual(saved.config, status.config)) {
+        saved = { hints: {} };
     }
 
     // Determine the current status for each hint.
     for (const category of results.categories) {
         for (const hint of category.hints) {
             const hintKey = `hint-${hint.id}`;
+            const savedCount = saved.hints[hintKey] || 0;
+
+            status.hints[hintKey] = hint.problems.length;
 
             if (hint.problems.length) {
-                hintStatus[hintKey] = HintStatus.failed;
-            } else if (prevStatus[hintKey] === HintStatus.failed) {
+                if (savedCount > hint.problems.length) {
+                    hintStatus[hintKey] = HintStatus.fixing;
+                } else {
+                    hintStatus[hintKey] = HintStatus.failed;
+                }
+            } else if (savedCount > 0) {
                 hintStatus[hintKey] = HintStatus.fixed;
             } else {
                 hintStatus[hintKey] = HintStatus.passed;
@@ -46,7 +61,7 @@ export const determineHintStatus = (config: Config, results: Results, s = storag
     }
 
     // Remember the current status *locally* for the next scan.
-    s.setItem(statusKey, { ...hintStatus, ...status });
+    s.setItem(statusKey, status);
 
     // Report only the status of hints to avoid logging user-provided data.
     return hintStatus;
