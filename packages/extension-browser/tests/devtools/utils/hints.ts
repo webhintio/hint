2eea04c1e0594ebbb3ba7768ccb7c5b97674d110
Tony Ross <antross@gmail.com>
2019-07-16 11:20:21 -0500

    New: Add fixing state to hint status metrics
    Tracks progress partially fixing issues reported by a hint.
    
    - - - - - - - - - -
    
    Fix #2678
    Close #2680

--- packages/extension-browser/tests/devtools/utils/hints.ts
+++ packages/extension-browser/tests/devtools/utils/hints.ts
@@ -16,15 +16,19 @@ const toPassResult = (id: string) => {
     return toHintResult(id, []);
 };
 
-const toFailResult = (id: string) => {
-    return toHintResult(id, [{ message: 'problem message' }]);
+const toFailResult = (id: string, count: number) => {
+    return toHintResult(id, new Array(count).fill({ message: 'problem message' }));
 };
 
-const mockResults = (url: string, pass: string[], fail: string[]) => {
+const mockResults = (url: string, pass: string[], fail: string[], nFailures = 1) => {
+    const failures = fail.map((id) => {
+        return toFailResult(id, nFailures);
+    });
+
     return {
         categories: [
             {
-                hints: [...pass.map(toPassResult), ...fail.map(toFailResult)],
+                hints: [...pass.map(toPassResult), ...failures],
                 name: Category.compatibility,
                 passed: pass.length
             }
@@ -77,6 +81,32 @@ test('It marks passed hints as fixed with failed history', (t) => {
 
     t.is(status['hint-compat-api/css'], HintStatus.passed);
     t.is(status['hint-compat-api/html'], HintStatus.fixed);
+});
+
+test('It marks failed hints as fixing with improvement vs history', (t) => {
+    const config = {};
+    const prevResults = mockResults('https://example.com', ['compat-api/css'], ['compat-api/html'], 3);
+    const results = mockResults('https://example.com', ['compat-api/css'], ['compat-api/html'], 2);
+    const storage = mockStorage();
+
+    determineHintStatus(config, prevResults, storage);
+    const status = determineHintStatus(config, results, storage);
+
+    t.is(status['hint-compat-api/css'], HintStatus.passed);
+    t.is(status['hint-compat-api/html'], HintStatus.fixing);
+});
+
+test('It marks failed hints as failed with no improvement', (t) => {
+    const config = {};
+    const prevResults = mockResults('https://example.com', ['compat-api/css'], ['compat-api/html'], 2);
+    const results = mockResults('https://example.com', ['compat-api/css'], ['compat-api/html'], 2);
+    const storage = mockStorage();
+
+    determineHintStatus(config, prevResults, storage);
+    const status = determineHintStatus(config, results, storage);
+
+    t.is(status['hint-compat-api/css'], HintStatus.passed);
+    t.is(status['hint-compat-api/html'], HintStatus.failed);
 });
 
 test('It marks previously failed hints as passed with changed url', (t) => {
