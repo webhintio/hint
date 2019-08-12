b0d618f3d61137eaab2a4fa4f7c16d655874f420
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2019-08-12 13:09:13 -0700

    Fix: Update localization to use the path instead of the package name

--- packages/extension-browser/src/shims/get-message.ts
+++ packages/extension-browser/src/shims/get-message.ts
@@ -4,8 +4,16 @@ import { getMessage as extensionGetMessage } from '../devtools/utils/i18n';
 
 type GetMessage = typeof import('@hint/utils/dist/src/i18n/get-message').getMessage;
 
-export const getMessage: GetMessage = (key, packageName, options?) => {
-    const prefix = camelCase(packageName.replace('@hint/', ''));
+export const getMessage: GetMessage = (key, path, options?) => {
+    /*
+     * Path will be something like:
+     * ..\hint-axe\dist\src
+     * or
+     * ../hint-axe/dist/src
+     */
+    const pathParts = path.split(/\\|\//g);
+    const packageName = pathParts[pathParts.length - 3];
+    const prefix = camelCase(packageName);
     const substitutions = options && options.substitutions;
 
     return extensionGetMessage(`${prefix}_${key}` as any, substitutions);
