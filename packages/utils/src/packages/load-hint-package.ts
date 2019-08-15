23300153b8798bc10e70e0e1ab96eed35ccef069
Jesus David García Gomez <jdgarcia@outlook.com>
2019-08-15 15:34:00 -0700

    Fix: Error building create-hint release package
    Close #2819

--- packages/utils/src/packages/load-hint-package.ts
+++ packages/utils/src/packages/load-hint-package.ts
@@ -3,7 +3,7 @@ export const loadHintPackage = () => {
     // webpack will embed the package.json
     /* istanbul ignore if */
     if (process.env.webpack) { // eslint-disable-line no-process-env
-        return require('../../../hint/package.json');
+        return require('../../../../hint/package.json');
     }
 
     return require(`hint/package.json`);
