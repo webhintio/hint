27fca6a25749f6c4d5f72d42c0f2e1bb9d3d39a0
Tony Ross <antross@gmail.com>
2019-08-27 13:30:09 -0500

    Chore: Fix test coverage

--- packages/extension-browser/tests/content-script/fetcher.ts
+++ packages/extension-browser/tests/content-script/fetcher.ts
@@ -1,10 +1,12 @@
 import * as proxyquire from 'proxyquire';
 import * as sinon from 'sinon';
 import test from 'ava';
-import { FetchEnd } from 'hint/dist/src/lib/types';
+import { FetchEnd, FetchStart } from 'hint/dist/src/lib/types';
 
 test('Fetcher can match requests to responses', async (t) => {
     const resource = 'http://example.com/site.manifest';
+    const missFetchStart = { resource: 'http://example.com/index.htm' } as FetchStart;
+    const matchFetchStart = { resource } as FetchStart;
     const missFetchEnd = { request: {}, resource: 'http://example.com/index.htm', response: {} } as FetchEnd;
     const matchFetchEnd = { request: {}, resource, response: { body: { content: 'test content' }} } as FetchEnd;
     const fetchStub = sinon.stub().resolves({});
@@ -21,13 +23,43 @@ test('Fetcher can match requests to responses', async (t) => {
 
     const promise = fetcher.fetch(resource);
 
+    const missStartHandled = fetcher.handle({ fetchStart: missFetchStart });
     const missHandled = fetcher.handle({ fetchEnd: missFetchEnd });
+    const matchStartHandled = fetcher.handle({ fetchStart: matchFetchStart });
     const matchHandled = fetcher.handle({ fetchEnd: matchFetchEnd });
 
     const result = await promise;
 
     t.true(fetchStub.calledOnce);
+    t.false(missStartHandled);
     t.false(missHandled);
+    t.true(matchStartHandled);
     t.true(matchHandled);
     t.deepEqual(result, matchFetchEnd);
+});
+
+test('Fetcher returns non-CORS responses directly', async (t) => {
+    const resource = 'http://example.com/site.manifest';
+    const fetchStub = sinon.stub().resolves({
+        headers: { forEach() {} },
+        text: () => {
+            return Promise.resolve('stub content');
+        },
+        type: 'basic'
+    });
+    const { Fetcher } = proxyquire(
+        '../../src/content-script/fetcher', {
+            '../shared/globals': {
+                '@noCallThru': true,
+                fetch: fetchStub
+            }
+        }
+    ) as typeof import('../../src/content-script/fetcher');
+
+    const fetcher = new Fetcher();
+
+    const result = await fetcher.fetch(resource);
+
+    t.true(fetchStub.calledOnce);
+    t.is(result.response.body.content, 'stub content');
 });
