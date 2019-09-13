d185ac5e12421472b44c66044de10355e228e6e8
Tony Ross <antross@gmail.com>
2019-09-12 23:10:53 -0500

    New: Use user-friendly browser names/versions in reports
    Names are pulled from mdn-browser-compat-data.
    Versions now focus on when a feature was added/removed.
    
    - - - - - - - - - -
    
    Fix #2896

--- packages/hint-compat-api/tests/html.ts
+++ packages/hint-compat-api/tests/html.ts
@@ -21,11 +21,11 @@ testHint(hintPath,
             name: 'Reports unsupported HTML attributes',
             reports: [
                 {
-                    message: 'img[srcset] is not supported by ie 10-11.',
+                    message: 'img[srcset] is not supported by Internet Explorer.',
                     position: { match: 'img srcset=' }
                 },
                 {
-                    message: 'div[hidden] is not supported by ie 10.',
+                    message: 'div[hidden] is not supported by Internet Explorer < 11.',
                     position: { match: 'div hidden' }
                 }
             ],
@@ -35,11 +35,11 @@ testHint(hintPath,
             name: 'Reports unsupported HTML elements',
             reports: [
                 {
-                    message: 'blink is not supported by chrome 73-74, edge 17-18, firefox 65-66, ie 10-11.',
+                    message: 'blink is not supported by Chrome, Edge, Firefox 22+, Internet Explorer.',
                     position: { match: 'blink' }
                 },
                 {
-                    message: 'details is not supported by edge 17-18, ie 10-11.',
+                    message: 'details is not supported by Edge, Internet Explorer.',
                     position: { match: 'details' }
                 }
             ],
@@ -54,7 +54,7 @@ testHint(hintPath,
             reports: [
                 // TODO: Include <form method="dialog"> or similar once MDN data is available
                 {
-                    message: 'input[type=color] is not supported by ie 10-11.',
+                    message: 'input[type=color] is not supported by Internet Explorer.',
                     position: { match: 'input type="color"' }
                 }
             ],
@@ -70,7 +70,7 @@ testHint(hintPath,
             name: 'Reports overridden ignored HTML features',
             reports: [
                 {
-                    message: 'script[integrity] is not supported by ie 10-11.',
+                    message: 'script[integrity] is not supported by Internet Explorer.',
                     position: { match: 'script integrity' }
                 }
             ],
