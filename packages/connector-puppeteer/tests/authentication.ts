850847560b6149d7ef699202ace270125dec0cc3
Antón Molleda <amolleda@gmail.com>
2019-09-14 19:34:36 -0700

    New: Support for Basic HTTP Auth
    Fix #1750

--- packages/connector-puppeteer/tests/authentication.ts
+++ packages/connector-puppeteer/tests/authentication.ts
@@ -13,7 +13,7 @@ import { validEvent } from './_valid-event';
 const { generateHTMLPage } = testUtils;
 const name = 'puppeteer';
 
-test(`[${name}] Authenticate on a page works`, async (t) => {
+test(`[${name}] Form authentication on a page works`, async (t) => {
     const user = 'user';
     const password = 'password';
 
@@ -157,6 +157,71 @@ test(`[${name}] Authenticate on a multi-step page works`, async (t) => {
                 selector: '#user',
                 value: user
             }
+        },
+        detached: true
+    });
+
+    await connector.collect(new URL(`http://localhost:${server.port}/`));
+
+    const updatedEvents = events.map((event) => {
+        return Server.updateLocalhost(event, server.port);
+    });
+    const invokes: any[] = [];
+
+    for (let i = 0; i < engineEmitAsyncSpy.callCount; i++) {
+        invokes.push(engineEmitAsyncSpy.getCall(i).args);
+    }
+
+    updatedEvents.forEach((event) => {
+        t.true(validEvent(invokes, event), `Event ${event[0]}/${event[1].resource} has the same properties`);
+    });
+
+    engineEmitAsyncSpy.restore();
+
+    await Promise.all([connector.close(), server.stop()]);
+});
+
+test(`[${name}] Basic HTTP Authentication works`, async (t) => {
+    const user = 'user';
+    const password = 'password';
+
+    const events = [
+        [
+            'fetch::start::target',
+            { resource: 'http://localhost/' }
+        ],
+        [
+            'fetch::end::html',
+            { response: { statusCode: 200 } }
+        ]
+    ];
+
+    const engine: Engine<Events> = {
+        emit(): boolean {
+            return false;
+        },
+        async emitAsync(): Promise<any> { },
+        on(): Engine {
+            return null as any;
+        },
+        timeout: 10000
+    } as any;
+
+    const server = await Server.create({
+        configuration: {
+            '/': {
+                auth: `Basic ${Buffer.from(`${user}:${password}`, 'utf-8').toString('base64')}`,
+                content: generateHTMLPage('', 'Success!')
+            }
+        }
+    });
+
+    const engineEmitAsyncSpy = sinon.spy(engine, 'emitAsync');
+
+    const connector = new Connector(engine, {
+        auth: {
+            password,
+            user
         },
         detached: true
     });
