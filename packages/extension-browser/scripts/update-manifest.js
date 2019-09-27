89018420829d303600816840c636307b175d1870
Antón Molleda <amolleda@gmail.com>
2019-09-27 05:26:29 -0700

    New: Add browser action
    Fix #3041

--- packages/extension-browser/scripts/update-manifest.js
+++ packages/extension-browser/scripts/update-manifest.js
@@ -4,6 +4,8 @@ const manifest = require('../dist/bundle/manifest.json');
 
 // Remove the "persistent" flag when building for Firefox to avoid a warning.
 delete manifest.background.persistent;
+// Remove the "browser_action" property to avoid displaying the logo close to the address bar on Firefox
+delete manifest.browser_action;
 
 const filename = path.resolve(`${__dirname}/../dist/bundle/manifest.json`);
 const content = JSON.stringify(manifest, null, 2);
@@ -13,5 +15,6 @@ fs.writeFile(filename, content, (err) => {
         throw err;
     } else {
         console.log(`Removed \`background.persistent=false\` from: ${filename}`);
+        console.log(`Removed \`browser_action\` from: ${filename}`);
     }
 });
