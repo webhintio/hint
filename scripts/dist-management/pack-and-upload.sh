56e1aa4e074285d6de1e2738abe4880efa958057
Antón Molleda <molant@users.noreply.github.com>
2019-10-14 14:35:10 -0700

    Fix: Narrowing pattern
    Co-Authored-By: Tony Ross <antross@gmail.com>

--- scripts/dist-management/pack-and-upload.sh
+++ scripts/dist-management/pack-and-upload.sh
@@ -2,6 +2,6 @@
 
 declare -r HASH="$(git rev-parse --verify HEAD)"
 
-find packages -type f -path '*dist*' | zip "$HASH" -@
+find packages -type f -path '*/dist/*' | zip "$HASH" -@
 
 node ./scripts/dist-management/upload-dist.js "$HASH.zip"
