55e401373160937828a193991eae5e60088d7a8a
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2019-10-16 09:43:38 -0700

    Fix: Tests

--- packages/hint-typescript-config/tests/no-comments.ts
+++ packages/hint-typescript-config/tests/no-comments.ts
@@ -21,7 +21,7 @@ const tests: HintLocalTest[] = [
         path: path.join(__dirname, 'fixtures', 'extends-with-error'),
         reports: [{
             message: 'The compiler option "removeComments" should be enabled to reduce the output size.',
-            position: { match: 'extends' }
+            position: { match: 'compilerOptions' }
         }]
     }
 ];
