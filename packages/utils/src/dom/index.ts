751b60a7ca3ed4c459e5f62a1b2ca863b42cc5d3
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2019-10-24 09:10:41 -0700

    Breaking: Allow flattened imports from utils
    Fix #2444

--- packages/utils/src/dom/index.ts
+++ packages/utils/src/dom/index.ts
@@ -1,5 +1,6 @@
 export * from './create-html-document';
 export * from './find-original-element';
 export * from './get-element-by-url';
-export * from './traverse';
 export * from './html';
+export * from './snapshot';
+export * from './traverse';
