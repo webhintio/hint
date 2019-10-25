c61a2db55a6804f3f8365d128785a5e374c07c96
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2019-10-25 14:55:38 -0700

    New: Common types package

---
+++ packages/utils-types/src/category.ts
@@ -0,0 +1,10 @@
+export enum Category {
+    accessibility = 'accessibility',
+    development = 'development',
+    compatibility = 'compatibility',
+    other = 'other',
+    pwa = 'pwa',
+    performance = 'performance',
+    pitfalls = 'pitfalls',
+    security = 'security'
+}
