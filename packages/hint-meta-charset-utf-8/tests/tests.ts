d8827f569291234bc8f8729b6f1f3b53bbf7067b
Antón Molleda <amolleda@gmail.com>
2019-11-02 20:59:16 -0700

    New: Add default severity to reports
    Fix #3231

--- packages/hint-meta-charset-utf-8/tests/tests.ts
+++ packages/hint-meta-charset-utf-8/tests/tests.ts
@@ -1,11 +1,13 @@
 import { generateHTMLPage } from '@hint/utils-create-server';
 import { getHintPath, HintTest, testHint } from '@hint/utils-tests-helpers';
+import { Severity } from '@hint/utils-types';
 
 const metaCharset = '<mEtA CHaRseT="UtF-8">';
 const metaHttpEquiv = '<MeTa HTTP-EquiV="ConTent-Type" Content="TexT/HTML; CharSet=UtF-8">';
 
 const metaElementCanBeShorterErrorMessage = `'charset' meta element should be specified using shorter '<meta charset="utf-8">' form.`;
 const metaElementHasIncorrectValueErrorMessage = `'charset' meta element value should be 'utf-8', not 'utf8'.`;
+const metaElementHasIncorrectRandomValueErrorMessage = `'charset' meta element value should be 'utf-8', not 'random'.`;
 const metaElementIsNotFirstInHeadErrorMessage = `'charset' meta element should be the first thing in '<head>'.`;
 const metaElementIsNotInHeadErrorMessage = `'charset' meta element should be specified in the '<head>', not '<body>'.`;
 const metaElementIsNotNeededErrorMessage = `'charset' meta element is not needed as one was already specified.`;
@@ -14,18 +16,35 @@ const metaElementNotSpecifiedErrorMessage = `'charset' meta element was not spec
 const tests: HintTest[] = [
     {
         name: `'charset' meta element is not specified`,
-        reports: [{ message: metaElementNotSpecifiedErrorMessage }],
+        reports: [{
+            message: metaElementNotSpecifiedErrorMessage,
+            severity: Severity.warning
+        }],
         serverConfig: generateHTMLPage()
     },
     {
         name: `'http-equiv' meta element is specified`,
-        reports: [{ message: metaElementCanBeShorterErrorMessage }],
+        reports: [{
+            message: metaElementCanBeShorterErrorMessage,
+            severity: Severity.warning
+        }],
         serverConfig: generateHTMLPage(metaHttpEquiv)
     },
     {
-        name: `'charset' meta element is specified with a value different then 'utf-8'`,
-        reports: [{ message: metaElementHasIncorrectValueErrorMessage }],
+        name: `'charset' meta element is specified with a value of 'utf8' instead of 'utf-8'`,
+        reports: [{
+            message: metaElementHasIncorrectValueErrorMessage,
+            severity: Severity.warning
+        }],
         serverConfig: generateHTMLPage('<meta charset="utf8">')
+    },
+    {
+        name: `'charset' meta element is specified with a value of 'random' instead of 'utf-8'`,
+        reports: [{
+            message: metaElementHasIncorrectRandomValueErrorMessage,
+            severity: Severity.error
+        }],
+        serverConfig: generateHTMLPage('<meta charset="random">')
     },
     {
         name: `'charset' meta element is specified with the value of 'utf-8'`,
@@ -34,24 +53,47 @@ const tests: HintTest[] = [
     {
         name: `'charset' meta element is specified in the '<body>'`,
         reports: [
-            { message: metaElementIsNotFirstInHeadErrorMessage },
-            { message: metaElementIsNotInHeadErrorMessage }
+            {
+                message: metaElementIsNotFirstInHeadErrorMessage,
+                severity: Severity.hint
+            },
+            {
+                message: metaElementIsNotInHeadErrorMessage,
+                severity: Severity.error
+            }
         ],
         serverConfig: generateHTMLPage(undefined, metaCharset)
     },
     {
         name: `'charset' meta element is specified in '<head>' after another element`,
-        reports: [{ message: metaElementIsNotFirstInHeadErrorMessage }],
+        reports: [{
+            message: metaElementIsNotFirstInHeadErrorMessage,
+            severity: Severity.hint
+        }],
         serverConfig: generateHTMLPage('<title>test</title><meta charset="utf-8">')
     },
     {
         name: `'charset' meta element is specified in '<head>' after an HTML comment`,
-        reports: [{ message: metaElementIsNotFirstInHeadErrorMessage }],
+        reports: [{
+            message: metaElementIsNotFirstInHeadErrorMessage,
+            severity: Severity.hint
+        }],
         serverConfig: generateHTMLPage('<!-- test --><meta charset="utf-8">')
     },
     {
+        name: `'charset' meta element is specified in '<head>' after an HTML comment bigger than 1024 bytes`,
+        reports: [{
+            message: metaElementIsNotFirstInHeadErrorMessage,
+            severity: Severity.error
+        }],
+        serverConfig: generateHTMLPage(`<!-- test ${' '.repeat(1024)}--><meta charset="utf-8">`)
+    },
+    {
         name: `Multiple meta 'charset' elements are specified`,
-        reports: [{ message: metaElementIsNotNeededErrorMessage }],
+        reports: [{
+            message: metaElementIsNotNeededErrorMessage,
+            severity: Severity.warning
+        }],
         serverConfig: generateHTMLPage(`${metaCharset}${metaHttpEquiv}`)
     },
     {
@@ -71,7 +113,10 @@ const tests: HintTest[] = [
     },
     {
         name: `Meta 'charset' is injected after load, should fail`,
-        reports: [{ message: metaElementNotSpecifiedErrorMessage }],
+        reports: [{
+            message: metaElementNotSpecifiedErrorMessage,
+            severity: Severity.warning
+        }],
         serverConfig: generateHTMLPage(`<title>No charset</title>
         <script>
             var head = document.querySelector('head');
@@ -85,26 +130,6 @@ const tests: HintTest[] = [
         name: `Resource is not an HTML document`,
         serverConfig: { '/': { headers: { 'Content-Type': 'image/png' } } }
     }
-
-    /*
-     * TODO: Enable it once `jsdom` returns the correct content.
-     *     {
-     *         name: `The XML charset declaration is used'`,
-     *             reports: [
-     *                 { message: metaElementNotSpecifiedErrorMessage },
-     *                 { message: `Unneeded XML declaration: '<?xml version="1.0" encoding="ISO-8859-1"?>'.` }
-     *             ],
-     *             serverConfig:
-     * `<?xml version="1.0" encoding="ISO-8859-1"?>
-     * <!doctype html>
-     * <html lang="en">
-     *     <head>
-     *         <title>test</title>
-     *     </head>
-     *     <body></body>
-     * </html>`
-     *     }
-     */
 ];
 
 testHint(getHintPath(__filename), tests, { parsers: ['html'] });
