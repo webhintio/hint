9498132995fdf8dc3511118f385629022bed1c4d
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2019-11-04 10:05:35 -0800

    Breaking: Remove Network utils in favor of @hint/utils-network

--- packages/utils/src/network/is-http.ts
+++
@@ -1,6 +0,0 @@
-import { hasProtocol } from './has-protocol';
-
-/** Convenience function to check if a resource is served over HTTP. */
-export const isHTTP = (resource: string): boolean => {
-    return hasProtocol(resource, 'http:');
-};
