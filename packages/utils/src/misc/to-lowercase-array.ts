cdaa663cd977e3beabe39a664c9f5960c27efcba
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2019-11-06 16:31:08 -0800

    Breaking: Remove string utils in favor of @hint/utils-string

--- packages/utils/src/misc/to-lowercase-array.ts
+++
@@ -1,6 +0,0 @@
-/** Lower cases all the items of `list`. */
-export const toLowerCaseArray = (list: string[]): string[] => {
-    return list.map((e) => {
-        return e.toLowerCase();
-    });
-};
