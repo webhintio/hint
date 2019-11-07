56fe7fb270a5f20c6e38f50e5e0f8c1ce0f13e15
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2019-11-06 16:28:22 -0800

    New: Add string utils package

---
+++ packages/utils-string/tests/to-pascal-case.ts
@@ -0,0 +1,24 @@
+import test from 'ava';
+
+import { toPascalCase } from '../src';
+
+test(`toPascalCase returns a string in Pascal Case`, (t) => {
+    const expected = 'ThisIsAString';
+    const pascalCased = toPascalCase('this-is-a-string');
+
+    t.is(pascalCased, expected);
+});
+
+test(`toPascalCase returns a string in Pascal Case even if the input doesn't have delimiter`, (t) => {
+    const expected = 'Test';
+    const pascalCased = toPascalCase('test');
+
+    t.is(pascalCased, expected);
+});
+
+test(`toPascalCase returns an empty sting if passed`, (t) => {
+    const expected = '';
+    const pascalCased = toPascalCase('');
+
+    t.is(pascalCased, expected);
+});
