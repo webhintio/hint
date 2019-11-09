96cd5fb1316f6ca403743bcdad11a777ed813150
Antón Molleda <molant@users.noreply.github.com>
2019-11-08 22:05:54 -0800

    New: Default severity for `html-checker`
    Fix #3220

--- packages/hint-html-checker/tests/tests.ts
+++ packages/hint-html-checker/tests/tests.ts
@@ -4,6 +4,7 @@ import * as mock from 'mock-require';
 
 import { getHintPath, HintTest, testHint } from '@hint/utils-tests-helpers';
 import * as utilsNetwork from '@hint/utils-network';
+import { Severity } from '@hint/utils-types';
 
 const hintPath = getHintPath(__filename);
 const exampleUrl = 'https://empty.webhint.io/';
@@ -17,16 +18,16 @@ const noErrorMessages = {
     messages: []
 };
 
-// Response from the default checker that contains errors/warnings
+// Response from the default checker that contains errors/warnings, values modified to validate `toSeverity`
 const defaultCheckerMessages = {
     url: exampleUrl,
     messages: [
         {
-            type: 'info',
+            type: 'error',
             lastLine: 1,
             lastColumn: 3114,
             firstColumn: 3046,
-            subType: 'error',
+            subType: 'fatal',
             message: '“role="none"” is not yet supported in all browsers. Consider instead either using “role="presentation"” or “role="none presentation"”.',
             extract: 'stration"><img src="/images/iceberg-left.svg" id="iceberg1" alt="" role="none"> <img ',
             hiliteStart: 10,
@@ -48,7 +49,6 @@ const defaultCheckerMessages = {
             lastLine: 1,
             lastColumn: 3000,
             firstColumn: 2000,
-            subType: 'warning',
             message: 'Consider using the “h1” element as a top-level heading only (all “h1” elements are treated as top-level headings by many screen readers and other tools)',
             extract: '-section"><h1>example<',
             hiliteStart: 8,
@@ -119,13 +119,15 @@ const testsForDefaults: HintTest[] = [
             position: {
                 column: defaultCheckerMessages.messages[0].firstColumn,
                 line: defaultCheckerMessages.messages[0].lastLine
-            }
+            },
+            severity: Severity.error
         }, {
             message: defaultCheckerMessages.messages[1].message,
             position: {
                 column: defaultCheckerMessages.messages[1].firstColumn,
                 line: defaultCheckerMessages.messages[1].lastLine
-            }
+            },
+            severity: Severity.warning
         }],
         before() {
             htmlCheckerMock({ error: true });
@@ -142,7 +144,8 @@ const testsForIgnoreStringConfigs: HintTest[] = [
             position: {
                 column: defaultCheckerMessages.messages[0].firstColumn,
                 line: defaultCheckerMessages.messages[0].lastLine
-            }
+            },
+            severity: Severity.error
         }],
         before() {
             htmlCheckerMock({ error: true });
@@ -193,19 +196,22 @@ const testsForDetailsConfig: HintTest[] = [
             position: {
                 column: defaultCheckerMessages.messages[0].firstColumn,
                 line: defaultCheckerMessages.messages[0].lastLine
-            }
+            },
+            severity: Severity.error
         }, {
             message: defaultCheckerMessages.messages[1].message,
             position: {
                 column: defaultCheckerMessages.messages[1].firstColumn,
                 line: defaultCheckerMessages.messages[1].lastLine
-            }
+            },
+            severity: Severity.warning
         }, {
             message: defaultCheckerMessages.messages[2].message,
             position: {
                 column: defaultCheckerMessages.messages[2].firstColumn,
                 line: defaultCheckerMessages.messages[2].lastLine
-            }
+            },
+            severity: Severity.information
         }],
         before() {
             htmlCheckerMock({ error: true });
