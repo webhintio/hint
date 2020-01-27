1e14ec66b6e3c6f30b901dbe32e9815bbe0c4a07
Artur Mizera <arturmiz@users.noreply.github.com>
2020-01-27 23:44:39 +0100

    Fix: Emmiting html events instead of new ones

--- packages/parser-vue/src/parser.ts
+++ packages/parser-vue/src/parser.ts
@@ -2,21 +2,31 @@
  * @fileoverview webhint parser needed to analyze Vue single file components files.
  */
 
-import { FetchEnd, Parser } from 'hint/dist/src/lib/types';
+import { debug as d } from '@hint/utils-debug';
+import { Parser } from 'hint/dist/src/lib/types';
 import { Engine } from 'hint/dist/src/lib/engine';
+import { HTMLEvents } from '@hint/parser-html';
+import { ScriptEvents } from '@hint/parser-javascript';
+// import { HTMLDocument } from '@hint/utils-dom';
 
-import { VueEvents } from './types';
+const debug = d(__filename);
 
-export * from './types';
-
-export default class VueParser extends Parser<VueEvents> {
-    public constructor(engine: Engine<VueEvents>) {
+export default class VueParser extends Parser<HTMLEvents> {
+    public constructor(engine: Engine<HTMLEvents & ScriptEvents>) {
         super(engine, 'vue');
 
-        engine.on('fetch::end::unknown', async ({ resource }: FetchEnd) => {
-            await engine.emitAsync(`parse::start::vue`, { resource });
+        engine.on('fetch::end::unknown', async ({ resource }) => {
+            if (!resource.endsWith('.vue')) {
+                return;
+            }
 
-            await engine.emitAsync(`parse::end::vue`, { resource });
+            debug(`Parsing Vue file: ${resource}`);
+
+            await engine.emitAsync(`parse::start::html`, { resource });
+
+            // const document = new HTMLDocument({}, resource);
+
+            // await engine.emitAsync(`parse::end::html`, { document, html: '', resource });
         });
     }
 }
--- packages/parser-vue/tests/parser.ts
+++ packages/parser-vue/tests/parser.ts
@@ -3,27 +3,29 @@ import test from 'ava';
 import { EventEmitter2 } from 'eventemitter2';
 import { FetchEnd } from 'hint/dist/src/lib/types';
 import { Engine } from 'hint/dist/src/lib/engine';
+import { HTMLEvents } from '@hint/parser-html';
+import { ScriptEvents } from '@hint/parser-javascript';
 
 import * as VueParser from '../src/parser';
-import { VueEvents } from '../src/types';
+// import { VueEvents } from '../src/types';
 
 test('If `fetch::end::unknown` is received, then the code should be parsed and the `parse::end::vue` event emitted', async (t) => {
     const sandbox = sinon.createSandbox();
-    const engine: Engine<VueEvents> = new EventEmitter2({}) as Engine<VueEvents>;
+    const engine: Engine<HTMLEvents & ScriptEvents> = new EventEmitter2({}) as Engine<HTMLEvents & ScriptEvents>;
 
     new VueParser.default(engine); // eslint-disable-line
 
     const engineEmitAsyncSpy = sandbox.spy(engine, 'emitAsync');
 
     await engine.emitAsync('fetch::end::unknown', {
-        resource: {},
+        resource: 'some.vue',
         response: {}
     } as FetchEnd);
 
     const args = engineEmitAsyncSpy.args;
 
-    t.is(args[1][0], 'parse::start::vue');
-    t.is(args[2][0], 'parse::end::vue');
+    t.is(args[1][0], 'parse::start::html');
+    // t.is(args[2][0], 'parse::end::html');
 
     sandbox.restore();
 });
