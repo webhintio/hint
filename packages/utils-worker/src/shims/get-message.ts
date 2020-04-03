9fe748bd742958b2d4c95c6ea0c237901e09ddd7
Tony Ross <antross@gmail.com>
2020-04-03 13:59:33 -0500

    Chore: Remove debugging console logs

--- packages/utils-worker/src/shims/get-message.ts
+++ packages/utils-worker/src/shims/get-message.ts
@@ -16,12 +16,6 @@ export const getMessage: GetMessage = (key, path, options?) => {
     const prefix = camelCase(packageName);
     const substitutions = options && options.substitutions;
     const name = `${prefix}_${key}`;
-
-    if (!(messages as any)[name]) {
-        console.log('Name', name);
-        console.log('Message', messages);
-    }
-
     const message: string = (messages as any)[name].message;
 
     return message.replace(/\$(\d+)/g, (match, number) => {
