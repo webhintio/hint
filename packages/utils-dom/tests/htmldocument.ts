14908ca1bdbb1c186bd8f3a026059b0ac5268dc0
John Emau <John.Emau@microsoft.com>
2020-07-10 09:53:55 -0700

    Chore: Add querySelector tests to utils-dom

--- packages/utils-dom/tests/htmldocument.ts
+++ packages/utils-dom/tests/htmldocument.ts
@@ -11,3 +11,21 @@ test('title', (t) => {
     t.is(doc2.title, '');
     t.is(doc3.title, 'test');
 });
+
+test('querySelectorAll', (t) => {
+    const doc = createHTMLDocument('<div id="d1">div1</div><div id="d2">div2</div>', 'http://localhost/');
+
+    t.is(doc.querySelectorAll('div').length, 2);
+    t.is(doc.querySelectorAll('#d1').length, 1);
+    t.is(doc.querySelectorAll('#d2').length, 1);
+    t.is(doc.querySelectorAll('#d1')[0].textContent, 'div1');
+    t.is(doc.querySelectorAll('#d2')[0].textContent, 'div2');
+    t.is(doc.querySelectorAll('div')[0], doc.querySelectorAll('#d1')[0]);
+    t.is(doc.querySelectorAll('div')[1], doc.querySelectorAll('#d2')[0]);
+});
+
+test('querySelector', (t) => {
+    const doc = createHTMLDocument('<div>div1</div>', 'http://localhost/');
+
+    t.is(doc.querySelector('div').textContent, 'div1');
+});
