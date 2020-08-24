7cdb48fa38b6b19d35730094ecea3626e4b4aed0
Jesus David Garcia Gomez <jegarc@microsoft.com>
2020-08-24 13:12:05 -0700

    Chore: Fix WSL test expectation

--- packages/utils/tests/misc/get-platform.ts
+++ packages/utils/tests/misc/get-platform.ts
@@ -1,9 +1,11 @@
 import test from 'ava';
 
+const isWsl: boolean = require('is-wsl');
+
 import { getPlatform } from '../../src/misc';
 
 test(`getPlatform returns the current platform`, (t) => {
     const platform = getPlatform();
 
-    t.is(platform, process.platform);
+    t.is(platform, isWsl ? 'wsl' : process.platform);
 });
