829b3d74254cacf45120430998d1746f7eaef59d
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-09-10 09:53:03 +0530

    New: Added pdf printing from scan result (browser extension)

---
+++ packages/extension-browser/src/devtools/views/controls/print-link.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import * as styles from './print-link.css';
+
+const onPrintHandler = () => window.print();
+
+const PrintLink = () => {
+    return (
+        <span className={styles.root} onClick={onPrintHandler}>
+            Print
+        </span>
+    );
+};
+
+export default PrintLink;
