a41a021b7b039895167ccee16eaf01329425c7e2
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-09-10 10:05:49 +0530

    Fix: Styling fix in print-link.tsx (browser extension)

--- packages/extension-browser/src/devtools/views/controls/print-link.tsx
+++ packages/extension-browser/src/devtools/views/controls/print-link.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 
 import * as styles from './print-link.css';
 
-const onPrintHandler = () => window.print();
+const onPrintHandler = () => {
+    window.print();
+};
 
 const PrintLink = () => {
     return (
