a5a9753309495152d6a9039931e9977971719401
Patrick Brosset <patrick.brosset@microsoft.com>
2020-11-04 23:55:08 -0800

    New: IE flexbox compat hint
    Hint that warns users when they do configure IE as a target browser and
    flexbox is used in one of the stylesheets.
    
    - - - - - - - - - - - - - - - - - - - -
     
    Fix: #4072
    Close #4073

---
+++ packages/hint-ie-flexbox-compat/src/hint.ts
@@ -0,0 +1,69 @@
+import { HintContext } from 'hint/dist/src/lib/hint-context';
+import { IHint } from 'hint/dist/src/lib/types';
+import { getFullCSSCodeSnippet, getCSSLocationFromNode } from '@hint/utils-css';
+import { StyleEvents } from '@hint/parser-css';
+import { Severity } from '@hint/utils-types';
+
+import meta from './meta';
+import { getMessage } from './i18n.import';
+
+/*
+ * ------------------------------------------------------------------------------
+ * Public
+ * ------------------------------------------------------------------------------
+ */
+
+export default class IEFlexboxCompatHint implements IHint {
+    public static readonly meta = meta;
+
+    public constructor(context: HintContext<StyleEvents>) {
+        if (!context.targetedBrowsers.some((browser) => {
+            return browser.toLowerCase().startsWith('ie ');
+        })) {
+            return;
+        }
+
+        let reported = false;
+
+        context.on('scan::end', () => {
+            reported = false;
+        });
+
+        context.on('parse::end::css', ({ ast, element, resource }) => {
+            if (reported) {
+                return;
+            }
+
+            ast.walkRules((rule) => {
+                if (reported) {
+                    return;
+                }
+
+                rule.each((declaration) => {
+                    if (reported) {
+                        return;
+                    }
+
+                    if (!('prop' in declaration)) {
+                        return;
+                    }
+
+                    if (declaration.prop !== 'display' || !declaration.value.endsWith('flex')) {
+                        return;
+                    }
+
+                    const codeSnippet = getFullCSSCodeSnippet(declaration);
+                    const message = getMessage('usingFlexWithIE', context.language);
+                    const location = getCSSLocationFromNode(declaration, { isValue: true });
+                    const severity = Severity.warning;
+
+                    context.report(
+                        resource,
+                        message,
+                        { codeLanguage: 'css', codeSnippet, element, location, severity });
+                    reported = true;
+                });
+            });
+        });
+    }
+}
