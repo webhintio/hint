96b52877d0e2a57d702670cf097c0dd09d8deaab
Tony Ross <antross@gmail.com>
2020-12-03 16:25:06 -0600

    Chore: Adapt to css-select breaking changes

--- packages/utils-dom/src/get-compiled-selector.ts
+++ packages/utils-dom/src/get-compiled-selector.ts
@@ -1,13 +1,13 @@
-import { compile, CompiledQuery } from 'css-select';
+import { compile } from 'css-select';
 
 // TODO: Use quick-lru so that it doesn't grow without bounds
-const CACHED_CSS_SELECTORS: Map<string, CompiledQuery> = new Map();
+const CACHED_CSS_SELECTORS: Map<string, ReturnType<typeof compile>> = new Map();
 
 /**
  * Helper to get a compiled cached css-select query function.
  * @param selector css selector
  */
-export const getCompiledSelector = (selector: string): CompiledQuery => {
+export const getCompiledSelector = (selector: string) => {
     if (!CACHED_CSS_SELECTORS.has(selector)) {
         CACHED_CSS_SELECTORS.set(selector, compile(selector));
     }
