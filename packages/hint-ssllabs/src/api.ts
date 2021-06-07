27d424cd42cabee38c224e07ef178b4706eb7355
Jesus David Garcia Gomez <jegarc@microsoft.com>
2021-06-07 03:19:56 -0700

    Fix: Resolve vulnerabilities

---
+++ packages/hint-ssllabs/src/api.ts
@@ -0,0 +1,26 @@
+/* istanbul ignore file */
+
+import got from 'got';
+import { SSLLabsOptions, SSLLabsResult } from './types';
+
+const APIURL = 'https://api.ssllabs.com/api/v3/analyze';
+
+const analyzeWithRetry = async (options: SSLLabsOptions): Promise<SSLLabsResult> => {
+    const result = await got(APIURL, { searchParams: options }).json<SSLLabsResult>();
+
+    if (result.status === 'READY' || result.status === 'ERROR') {
+        return result;
+    }
+
+    return analyzeWithRetry(options);
+};
+
+export const analyze = async (options: SSLLabsOptions): Promise<SSLLabsResult> => {
+    const result = await analyzeWithRetry(options);
+
+    if (result.status === 'ERROR') {
+        throw new Error(`Error analyzing url ${options.host}`);
+    }
+
+    return result;
+};
