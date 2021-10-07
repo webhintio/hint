67cfb1becc1e9e8fbc2d8da9ae1b999e06d8b3f7
Jesus David Garcia Gomez <jegarc@microsoft.com>
2021-10-07 11:42:26 -0700

    Fix: Add screen to globals

--- packages/utils-worker/src/shared/globals.ts
+++ packages/utils-worker/src/shared/globals.ts
@@ -2,9 +2,14 @@ type Scope = {
     addEventListener(name: 'message', handler: (event: MessageEvent) => void): void;
     postMessage(data: any): void;
     removeEventListener(name: 'message', handler: (event: MessageEvent) => void): void;
+    screen: {};
 }
 
 // Include references to web worker globals to facilitate mocks during testing.
 const _self: Scope = self as any;
+
+if (!_self.screen) {
+    _self.screen = {};
+}
 
 export { _self as self };
