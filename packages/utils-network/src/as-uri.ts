81e56e98122c0ddba909220494b5c53bd7c9f0ca
Tony Ross <785009+antross@users.noreply.github.com>
2021-10-28 14:38:03 -0500

    Fix: Restore handling relative file paths in getAsUri
    This was regressed when attempting to simplify dependencies.
    An incorrect assumption was made about passed paths already
    being absolute, but in the CLI relative paths can make it here.
    
    Fixed by resolving the passed paths before converting to a URL.
    Also added a new test to validate relative paths are converted.
    
    - - - - - - - - - - - - - - - - - - - -
    
    Fix #4836

--- packages/utils-network/src/as-uri.ts
+++ packages/utils-network/src/as-uri.ts
@@ -1,3 +1,4 @@
+import * as path from 'path';
 import * as url from 'url';
 import { URL } from 'url'; // this is necessary to avoid TypeScript mixes types.
 
@@ -45,7 +46,7 @@ export const getAsUri = (source: string): URL | null => {
      * If it does exist and it's a regular file.
      */
     if (isFile(source) || isDirectory(source)) {
-        target = new URL(`file://${source}`);
+        target = new URL(`file://${path.resolve(source)}`);
         debug(`Adding valid target: ${url.format(target)}`);
 
         return target;
