b3d2f214df406b57345f8aa310e696c92d10cb6b
Jake McLeman <jamclema@microsoft.com>
2022-02-11 13:32:32 -0800

    Fix: Ensure errors are thrown on invalid JSON files

--- packages/utils-fs/src/load-json-file.ts
+++ packages/utils-fs/src/load-json-file.ts
@@ -1,8 +1,18 @@
 import stripComments = require('strip-json-comments');
-import { parse } from 'jsonc-parser';
+import { parse, parseTree } from 'jsonc-parser';
 import { readFile } from './read-file';
 
 /** Loads a JSON a file. */
 export const loadJSONFile = (filePath: string) => {
-    return parse(stripComments(readFile(filePath)));
+    const jsonStr = stripComments(readFile(filePath));
+    const data = parse(jsonStr);
+    const root = parseTree(jsonStr);
+
+    // If we didn't get a root, it's invalid JSON. If there are no children, its also likely invalid JSON.
+    if (!root || !root.children || root.children.length === 0) {
+        // Use the built-in JSON parser to throw an error
+        JSON.parse(jsonStr);
+    }
+
+    return data;
 };
