1f092627e8457bc4e5a515676193fd441639242f
Tony Ross <785009+antross@users.noreply.github.com>
2022-06-16 13:10:42 -0700

    Fix: Limit element/attribute positions to start of name
    Adapts to expansion of underlying locations to the full range of the
    element/attribute to facilitate having editors underline only the
    element/attribute name.

--- packages/hint/src/lib/hint-context.ts
+++ packages/hint/src/lib/hint-context.ts
@@ -128,14 +128,20 @@ export class HintContext<E extends Events = Events> {
     /** Finds the approximative location in the page's HTML for a match in an element. */
     public findProblemLocation(element: HTMLElement, offset: ProblemLocation | null, attribute?: string): ProblemLocation | null {
         if (attribute) {
-            return element.getAttributeLocation(attribute);
+            const { column, line, startOffset } = element.getAttributeLocation(attribute);
+
+            // Point to the just start of the attribute name (helps editors underline just the name).
+            return { column, line, startOffset };
         }
 
         if (offset) {
             return element.getContentLocation(offset);
         }
 
-        return element.getLocation();
+        const { column, line, startOffset } = element.getLocation();
+
+        // Point to the start of the element name (skipping '<', helps editors undeline just the name).
+        return { column: column + 1, line, startOffset };
     }
 
     /** Reports a problem with the resource. */
