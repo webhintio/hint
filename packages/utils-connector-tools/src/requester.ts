5a16461709b5b9cf925720706700fb5ea32f5e27
vidorteg <43078681+vidorteg@users.noreply.github.com>
2022-07-21 14:15:53 -0700

    Chore: default to false for rejectUnauthorized.

--- packages/utils-connector-tools/src/requester.ts
+++ packages/utils-connector-tools/src/requester.ts
@@ -64,6 +64,7 @@ const defaults: IRequestOptions = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
     },
     redirect: 'manual',
+    rejectUnauthorized: false,
     strictSSL: false,
     timeout: 30000
 };
