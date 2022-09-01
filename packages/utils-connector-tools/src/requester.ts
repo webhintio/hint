5d9d617375fd8cd332889144e71d2305dfea45dc
Vidal Ortega <43078681+vidorteg@users.noreply.github.com>
2022-08-31 17:52:59 -0700

    Fix: Updating the user-agent in the requester.
    Updates the user-agent for the requester class.
    ----
    Close #5287

--- packages/utils-connector-tools/src/requester.ts
+++ packages/utils-connector-tools/src/requester.ts
@@ -61,7 +61,7 @@ const defaults: IRequestOptions = {
         'Cache-Control': 'no-cache',
         DNT: '1',
         Pragma: 'no-cache',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 Edg/104.0.1293.70'
     },
     redirect: 'manual',
     rejectUnauthorized: false,
