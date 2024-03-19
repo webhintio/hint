45b9f4ea65dfdc21909539334d6d0bdad33e8a84
Vidal Guillermo Diazleal Ortega <43078681+vidorteg@users.noreply.github.com>
2024-03-19 05:21:34 +0000

    Fix: building required package used in prebuild step

--- packages/hint-axe/scripts/create/create-metas.js
+++ packages/hint-axe/scripts/create/create-metas.js
@@ -1,8 +1,97 @@
-const { Severity } = require('@hint/utils-types');
+const spawn = require('child_process').spawn;
+const globby = require('globby');
+const chalk = require('chalk');
+
 const { camelCase } = require('lodash');
 const { categoryId, escapeKey, mkdir, rulesIn, writeFile } = require('./utils');
 
 /** @typedef {import('axe-core').RuleMetadata} RuleMeta */
+
+/** Execute the `cmd` in a new process. */
+const exec = (cmd) => {
+    return new Promise((resolve, reject) => {
+        console.log(chalk.green(`  ${cmd}`));
+        const command = spawn(cmd, [], {
+            shell: true,
+            stdio: 'inherit'
+        });
+
+        command.on('error', (err) => {
+            return reject(err);
+        });
+
+        command.on('exit', (code) => {
+            if (code !== 0) {
+                return reject(new Error('NoExitCodeZero'));
+            }
+
+            return resolve(true);
+        });
+
+    });
+};
+
+/**
+ * Searches 'packages/utils-types' and returns
+ * an array with the path and the content of the result.
+ * @returns { {packagePath: string, content: string}[] } The packages information
+ */
+const getPackages = async () => {
+    const packagesPaths = await globby([
+        `../../**/packages/utils-types/package.json`
+    ], { absolute: true});
+
+    const packages = packagesPaths.map((packagePath) => {
+        try {
+            const content = require(packagePath);
+
+            return {
+                content,
+                packagePath
+            };
+        } catch (e) {
+            // This happens if we are creating a new package and switching branches. We can ignore
+            return {
+                content: '',
+                packagePath
+            };
+        }
+    });
+
+    return packages;
+};
+
+/**
+ * Executes `yarn build` for the given package if present
+ * @param { { packagePath: string, content: string } } pkg The package to test
+ * @returns { Promise<boolean> } True if `yarn build` executed correctly
+ * or if the package does not have that script.
+ */
+const prebuildPackage = async (pkg) => {
+    if (pkg.content
+    ) {
+        console.log(chalk.green(`\tPrebuilding...`));
+        const cwd = pkg.packagePath.replace('package.json', '');
+
+        return await exec(`cd ${cwd} && yarn build`);
+    }
+
+    return true;
+};
+
+const prebuildPackages = async () => {
+    const packages = await getPackages();
+    let ok = true;
+
+    for (const pkg of packages) {
+        console.log(chalk.green.bold(`Processing "${pkg.packagePath}"`));
+        ok = await prebuildPackage(pkg);
+
+        if (!ok) {
+            console.log(chalk.red.bold(`Error building package "${pkg.packagePath}"`));
+        }
+    }
+};
 
 /**
  * @param {string} category
@@ -10,6 +99,7 @@ const { categoryId, escapeKey, mkdir, rulesIn, writeFile } = require('./utils');
  */
 const createMeta = async (category, rules) => {
     const id = categoryId(category);
+    const { Severity } = await require('@hint/utils-types');
     const camelCaseId = camelCase(id);
     const rulesIds = rules.map((rule) => {
         return `'${rule.ruleId}'`;
@@ -81,6 +171,7 @@ export default meta;
  * @param {RuleMeta[]} rules
  */
 const createMetas = async (categories, rules) => {
+    await prebuildPackages();
     await mkdir('src/meta');
 
     for (const category of categories) {
